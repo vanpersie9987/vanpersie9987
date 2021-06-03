e887d3e06833ef375407c3be1b9f97539702ad27
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-06-03 21:34:55 +0800

    456. 132 模式

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -12327,4 +12327,21 @@ public class LeetCodeText {
         }
     }
 
+    // 456. 132 模式
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int k = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; --i) {
+            if (nums[i] < k) {
+                return true;
+            }
+            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
+                k = nums[stack.pop()];
+            }
+            stack.push(i);
+        }
+        return false;
+
+    }
+
 }
