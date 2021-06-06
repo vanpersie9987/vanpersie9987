4dfb2e73dbacce5619420fe696e574cde4e8ae6a
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-06-06 17:47:49 +0800

    剑指 Offer 09. 用两个栈实现队列 // 面试题 03.04. 化栈为队

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -12622,4 +12622,78 @@ public class LeetCodeText {
 
         return res.substring(0, res.length() - deleteK).toString();
     }
+
+    // 剑指 Offer 09. 用两个栈实现队列
+    class CQueue {
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+
+        public CQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            if (stack2.isEmpty()) {
+                return -1;
+            }
+            return stack2.pop();
+
+        }
+    }
+
+    // 面试题 03.04. 化栈为队
+    class MyQueue {
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+
+        /** Initialize your data structure here. */
+        public MyQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            stack1.push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.pop();
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.peek();
+
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
+    }
+
 }
