87efc8e208c92f23cd75a80f9d52bf405240fbcc
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-06-13 15:12:58 +0800

    385. 迷你语法分析器

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -13203,4 +13203,84 @@ public class LeetCodeText {
 
     }
 
+    // This is the interface that allows for creating nested lists. // You should
+    // not implement it, or speculate about its implementation
+    public class NestedInteger { // Constructor initializes an empty nested list.
+        public NestedInteger() {
+
+        }
+
+        public NestedInteger(int val) {
+
+        }
+
+        // Constructor initializes a single integer. public NestedInteger(int value);
+
+        // @return true if this NestedInteger holds a single integer, rather than a
+        // nested list.
+        public boolean isInteger() {
+            return false;
+        }
+
+        // @return the single integer that this NestedInteger holds, if it holds a
+        // single integer // Return null if this NestedInteger holds a nested list
+        public Integer getInteger() {
+            return null;
+        }
+
+        // Set this NestedInteger to hold a single integer.
+        public void setInteger(int value) {
+        }
+
+        // Set this NestedInteger to hold a nested list and adds a nested integer to
+        // it.
+        public void add(NestedInteger ni) {
+        }
+
+        // @return the nested list that this NestedInteger holds, if it holds a
+        // nested list
+        // Return null if this NestedInteger holds a single integer
+        public List<NestedInteger> getList() {
+            return null;
+
+        }
+    }
+
+    // 385. 迷你语法分析器
+    public NestedInteger deserialize(String s) {
+
+        if (s.charAt(0) != '[') {
+            return new NestedInteger(Integer.parseInt(s));
+        }
+        int i = 0;
+        Stack<NestedInteger> stack = new Stack<>();
+        while (i < s.length()) {
+            if (s.charAt(i) == '[') {
+                stack.push(new NestedInteger());
+            } else if (Character.isDigit(s.charAt(i)) || s.charAt(i) == '-') {
+                int sign = 1;
+                if (s.charAt(i) == '-') {
+                    sign = -1;
+                    ++i;
+                }
+                int num = 0;
+                while (Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    ++i;
+                }
+                --i;
+                stack.peek().add(new NestedInteger(sign * num));
+            } else if (s.charAt(i) == ']') {
+                if (stack.size() > 1) {
+                    NestedInteger nestedInteger = stack.pop();
+                    stack.peek().add(nestedInteger);
+                }
+            }
+            ++i;
+
+        }
+        return stack.pop();
+
+    }
+
 }
