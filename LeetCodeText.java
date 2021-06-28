dbd56b5a89c644447db3b024188bfa2826953e52
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-06-28 16:21:59 +0800

    优化

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -10419,18 +10419,11 @@ public class LeetCodeText {
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == '1') {
-                    grid[i][j] = '0';
                     if (i - 1 >= 0 && grid[i - 1][j] == '1') {
                         union.union(getIndex200(n, i, j), getIndex200(n, i - 1, j));
                     }
                     if (j - 1 >= 0 && grid[i][j - 1] == '1') {
                         union.union(getIndex200(n, i, j), getIndex200(n, i, j - 1));
-                    }
-                    if (i + 1 < m && grid[i + 1][j] == '1') {
-                        union.union(getIndex200(n, i, j), getIndex200(n, i + 1, j));
-                    }
-                    if (j + 1 < n && grid[i][j + 1] == '1') {
-                        union.union(getIndex200(n, i, j), getIndex200(n, i, j + 1));
                     }
                 }
             }
