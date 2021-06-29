425d95b1f81a62b0bd5e460ce9eaba2029bcd1a3
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-06-29 16:56:13 +0800

    剑指 Offer 06. 从尾到头打印链表

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -15006,4 +15006,19 @@ public class LeetCodeText {
 
     }
 
+    // 剑指 Offer 06. 从尾到头打印链表
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] res = new int[stack.size()];
+        int index = 0;
+        while (!stack.isEmpty()) {
+            res[index++] = stack.pop();
+        }
+        return res;
+    }
+
 }
