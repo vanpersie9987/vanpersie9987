fed01aa8ebbc6e592dd060eb8b0a7b934863149b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-06-30 23:03:52 +0800

    面试题 17.16. 按摩师 (类似打家劫舍)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -8606,6 +8606,23 @@ public class LeetCodeText {
         return dp[dp.length - 1];
     }
 
+    // 面试题 17.16. 按摩师 (类似打家劫舍)
+    public int massage(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; ++i) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[dp.length - 1];
+    }
+
     // 179. 最大数
     public String largestNumber(int[] nums) {
         String[] strings = new String[nums.length];
