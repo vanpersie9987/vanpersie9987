79d0d1b3e38e3edd30499b73a08947e48879025d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-06-30 22:08:35 +0800

    205. 同构字符串

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -15280,4 +15280,19 @@ public class LeetCodeText {
 
     }
 
+    // 205. 同构字符串
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            if ((map1.containsKey(s.charAt(i)) && map1.get(s.charAt(i)) != t.charAt(i))
+                    || (map2.containsKey(t.charAt(i)) && map2.get(t.charAt(i)) != s.charAt(i))) {
+                return false;
+            }
+            map1.put(s.charAt(i), t.charAt(i));
+            map2.put(t.charAt(i), s.charAt(i));
+        }
+        return true;
+    }
+
 }
