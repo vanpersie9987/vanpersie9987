afa8fd18b232ebac3068da0d70435eed37bc6f2c
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-02 18:01:05 +0800

    455. 分发饼干

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -15545,4 +15545,22 @@ public class LeetCodeText {
         return true;
 
     }
+
+    // 455. 分发饼干
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                ++ans;
+                ++i;
+            }
+            ++j;
+        }
+        return ans;
+
+    }
 }
