b554118ad0c3c3b3b772351382b3a8d683ffc6b3
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-04 12:38:21 +0800

    1526. 形成目标数组的子数组最少增加次数

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -15725,4 +15725,13 @@ public class LeetCodeText {
 
     }
 
+    // 1526. 形成目标数组的子数组最少增加次数
+    public int minNumberOperations(int[] target) {
+        int ans = target[0];
+        for (int i = 1; i < target.length; ++i) {
+            ans += Math.max(target[i] - target[i - 1], 0);
+        }
+        return ans;
+    }
+
 }
