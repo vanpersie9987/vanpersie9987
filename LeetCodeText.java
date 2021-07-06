04885ad60c5c4c06cbe9d97cfc4bdbffc088eebe
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-06 13:55:25 +0800

    412. Fizz Buzz

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -15961,4 +15961,22 @@ public class LeetCodeText {
 
     }
 
+    // 412. Fizz Buzz
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; ++i) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
+
+    }
+
 }
