147303ae92377e0bc8cba958ac6fec540f114042
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-07 21:41:12 +0800

    1041. 困于环中的机器人

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -16127,4 +16127,28 @@ public class LeetCodeText {
 
     }
 
+    // 1041. 困于环中的机器人
+    // 满足以下条件之一，会构成死循环
+    // 1、终点位于原点
+    // 2、终点的方向与起点不同
+    public boolean isRobotBounded(String instructions) {
+        int[][] directions = { { 0, 1 }, { -1, 0 }, { 0, -1 }, { 1, 0 } };
+        int d = 0;
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < instructions.length(); ++i) {
+            char c = instructions.charAt(i);
+            if (c == 'G') {
+                x += directions[d][0];
+                y += directions[d][1];
+            } else if (c == 'L') {
+                d = (d + 1) % directions.length;
+            } else {
+                d = (d - 1 + directions.length) % directions.length;
+            }
+        }
+        return (x == 0 && y == 0) || (d != 0);
+
+    }
+
 }
