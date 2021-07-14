f8659729a8ee8508ab1b6303dedcfad5eee440f3
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-15 00:30:39 +0800

    1323. 6 和 9 组成的最大数字

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -16474,4 +16474,17 @@ public class LeetCodeText {
         return x * 20001 + y;
     }
 
+    // 1323. 6 和 9 组成的最大数字
+    public int maximum69Number(int num) {
+        char[] chars = String.valueOf(num).toCharArray();
+        for (int i = 0; i < chars.length; ++i) {
+            if (chars[i] == '6') {
+                chars[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(String.valueOf(chars));
+
+    }
+
 }
