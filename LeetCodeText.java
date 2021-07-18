611153014651a39cb264e12f097f2e7aa0cb22c1
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-18 21:10:03 +0800

    1189. “气球” 的最大数量 (Maximum Number of Balloons)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -16569,4 +16569,39 @@ public class LeetCodeText {
 
     }
 
+    // 1189. “气球” 的最大数量 (Maximum Number of Balloons)
+    public int maxNumberOfBalloons(String text) {
+        int[] counts = new int[5];
+        for (char c : text.toCharArray()) {
+            switch (c) {
+                case 'b':
+                    ++counts[0];
+                    break;
+                case 'a':
+                    ++counts[1];
+                    break;
+                case 'l':
+                    ++counts[2];
+                    break;
+                case 'o':
+                    ++counts[3];
+                    break;
+                case 'n':
+                    ++counts[4];
+                    break;
+                default:
+                    break;
+            }
+        }
+        counts[2] /= 2;
+        counts[3] /= 2;
+        int res = Integer.MAX_VALUE;
+        for (int count : counts) {
+            res = Math.min(count, res);
+
+        }
+        return res;
+
+    }
+
 }
