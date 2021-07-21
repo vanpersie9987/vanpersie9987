b6c5149df84bdd9d75031b187d3a127f65109691
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-21 20:46:05 +0800

    1897. 重新分配字符使所有字符串都相等 (Redistribute Characters to Make All Strings Equal)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -16833,11 +16833,8 @@ public class LeetCodeText {
             }
         }
         for (int count : counts) {
-            if (count != 0) {
-                if (count % words.length != 0) {
-                    return false;
-                }
-
+            if (count % words.length != 0) {
+                return false;
             }
         }
         return true;
