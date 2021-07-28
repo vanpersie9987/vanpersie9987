d36c19bf9f18f495c0397b738ab775d1e3daa0ac
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-28 22:12:33 +0800

    1768.交替合并字符串 (Merge Strings Alternately)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -16908,4 +16908,21 @@ public class LeetCodeText {
 
     }
 
+    // 1768.交替合并字符串 (Merge Strings Alternately)
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                res.append(word1.charAt(i++));
+            }
+            if (j < word2.length()) {
+                res.append(word2.charAt(j++));
+            }
+        }
+        return res.toString();
+
+    }
+
 }
