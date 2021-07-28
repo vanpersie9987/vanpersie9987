3d3272e2fbb721ba95869d90ef5afa9e59de1bbb
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-28 21:19:41 +0800

    剑指 Offer 58 - II. 左旋转字符串

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -16824,4 +16824,30 @@ public class LeetCodeText {
 
     }
 
+    // 剑指 Offer 58 - II. 左旋转字符串
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder res = new StringBuilder();
+        for (int i = n; i < s.length(); ++i) {
+            res.append(s.charAt(i));
+        }
+        for (int i = 0; i < n; ++i) {
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+
+    // 剑指 Offer 58 - II. 左旋转字符串
+    public String reverseLeftWords2(String s, int n) {
+        char[] res = new char[s.length()];
+        for (int i = 0; i < s.length(); ++i) {
+            res[(i - n + s.length()) % s.length()] = s.charAt(i);
+        }
+        return new String(res);
+    }
+
+    // 剑指 Offer 58 - II. 左旋转字符串
+    public String reverseLeftWords3(String s, int n) {
+        return s.substring(n) + s.substring(0, n);
+    }
+
 }
