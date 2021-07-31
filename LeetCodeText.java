4b13d7d791fc3ad224d8b4005b9d9770ffa35e8b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-07-31 21:49:15 +0800

    881. 救生艇 (Boats to Save People)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -17155,4 +17155,20 @@ public class LeetCodeText {
 
     }
 
+    // 881. 救生艇 (Boats to Save People)
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int res = 0;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                ++left;
+            }
+            --right;
+            ++res;
+        }
+        return res;
+    }
+
 }
