f8db7675dfcb86ff99af397eccd6f931c523e710
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-05 18:20:58 +0800

    845. 数组中的最长山脉 (Longest Mountain in Array)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -17449,4 +17449,30 @@ public class LeetCodeText {
 
     }
 
+    // 845. 数组中的最长山脉 (Longest Mountain in Array)
+    public int longestMountain(int[] arr) {
+        int res = 0;
+        int left = 0;
+        while (left + 2 < arr.length) {
+            int right = left + 1;
+            if (arr[left] < arr[left + 1]) {
+                while (right + 1 < arr.length && arr[right] < arr[right + 1]) {
+                    ++right;
+                }
+                if (right + 1 < arr.length && arr[right] > arr[right + 1]) {
+                    while (right + 1 < arr.length && arr[right] > arr[right + 1]) {
+                        ++right;
+                    }
+                    res = Math.max(res, right - left + 1);
+                } else {
+                    ++right;
+                }
+            }
+
+            left = right;
+        }
+        return res;
+
+    }
+
 }
