0ef612c7ad160e3c7491bf416b25fd43ac8ffb26
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-08 21:21:19 +0800

    LCP 06. 拿硬币

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -17564,4 +17564,14 @@ public class LeetCodeText {
 
     }
 
+    // LCP 06. 拿硬币
+    public int minCount(int[] coins) {
+        int res = 0;
+        for (int coin : coins) {
+            res += (coin + 1) / 2;
+        }
+        return res;
+
+    }
+
 }
