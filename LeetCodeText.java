de5fec5b4cf73beba6578474ba84c184bbf5f0fe
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-11 10:56:17 +0800

    861. 翻转矩阵后的得分 (Score After Flipping Matrix)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -17765,4 +17765,25 @@ public class LeetCodeText {
         return right;
 
     }
+
+    // 861. 翻转矩阵后的得分 (Score After Flipping Matrix)
+    public int matrixScore(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = m * (1 << (n - 1));
+        for (int j = 1; j < n; ++j) {
+            int col = 0;
+            for (int i = 0; i < m; ++i) {
+                if (grid[i][0] == 1) {
+                    col += grid[i][j];
+                } else {
+                    col += 1 - grid[i][j];
+                }
+            }
+            col = Math.max(col, m - col);
+            res += col * (1 << (n - j - 1));
+        }
+        return res;
+
+    }
 }
