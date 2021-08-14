d2c175a52441626eb50ae73ac5e17ae4be69d150
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-14 11:56:29 +0800

    剑指 Offer 62. 圆圈中最后剩下的数字

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -17931,4 +17931,19 @@ public class LeetCodeText {
 
     }
 
+    // 剑指 Offer 62. 圆圈中最后剩下的数字
+    public int lastRemaining(int n, int m) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            list.add(i);
+        }
+        int index = 0;
+        while (list.size() != 1) {
+            index = (index + m - 1) % list.size();
+            list.remove(index);
+        }
+        return list.get(0);
+
+    }
+
 }
