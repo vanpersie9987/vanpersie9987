9ddec29ed49ea8f3829790996bf1b9f1360951ad
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-14 11:32:29 +0800

    剑指 Offer 17. 打印从1到最大的n位数

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -17920,4 +17920,15 @@ public class LeetCodeText {
 
     }
 
+    // 剑指 Offer 17. 打印从1到最大的n位数
+    public int[] printNumbers(int n) {
+        int count = (int) Math.pow(10, n) - 1;
+        int[] res = new int[count];
+        for (int i = 0; i < res.length; ++i) {
+            res[i] = i + 1;
+        }
+        return res;
+
+    }
+
 }
