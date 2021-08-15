dc78b19b6a8379058df0bf9ca656f18764775442
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-15 12:27:10 +0800

    11. 盛最多水的容器 (Container With Most Water)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -84,19 +84,20 @@ public class LeetCodeText {
 
     }
 
-    public int maxArea(final int[] height) {
-        int max = 0;
+    // 11. 盛最多水的容器 (Container With Most Water)
+    public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
+        int res = 0;
         while (left < right) {
-            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            res = Math.max(res, (right - left) * Math.min(height[left], height[right]));
             if (height[left] < height[right]) {
                 ++left;
             } else {
                 --right;
             }
         }
-        return max;
+        return res;
 
     }
 
