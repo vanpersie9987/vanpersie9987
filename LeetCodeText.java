f218690944ef74c452dfa34dfe70cafa6aaea3eb
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-18 18:02:33 +0800

    477. 汉明距离总和 (Total Hamming Distance)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -18088,4 +18088,18 @@ public class LeetCodeText {
 
     }
 
+    // 477. 汉明距离总和 (Total Hamming Distance)
+    public int totalHammingDistance(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < 30; ++i) {
+            int c = 0;
+            for (int num : nums) {
+                c += (num >> i) & 1;
+            }
+            res += c * (nums.length - c);
+        }
+        return res;
+
+    }
+
 }
