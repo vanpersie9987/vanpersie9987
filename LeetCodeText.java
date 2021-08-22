ccbe4d2550927c14b07b545b05eca186996e4073
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-22 17:14:09 +0800

    206. 反转链表 (Reverse Linked List)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -11,6 +11,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import javax.swing.ListModel;
+
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
@@ -18126,6 +18129,20 @@ public class LeetCodeText {
             }
         }
         return res;
+
+    }
+
+    // 206. 反转链表 (Reverse Linked List)
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
 
     }
 
