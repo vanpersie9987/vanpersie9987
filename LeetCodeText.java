dcf4e975edf7242b2133e58d8c8e2cb09f27c60b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-22 19:57:56 +0800

    1720. 解码异或后的数组 (Decode XORed Array)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -18206,4 +18206,14 @@ public class LeetCodeText {
 
     }
 
+    // 1720. 解码异或后的数组 (Decode XORed Array)
+    public int[] decode(int[] encoded, int first) {
+        int[] res = new int[encoded.length + 1];
+        res[0] = first;
+        for (int i = 1; i < res.length; ++i) {
+            res[i] = res[i - 1] ^ encoded[i - 1];
+        }
+        return res;
+    }
+
 }
