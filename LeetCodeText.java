88436f4693dbdb3f304e25fd3d6bd189d2ee2deb
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-25 21:12:45 +0800

    21. 合并两个有序链表 (Merge Two Sorted Lists)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -18408,4 +18408,23 @@ public class LeetCodeText {
 
     }
 
+    // 21. 合并两个有序链表 (Merge Two Sorted Lists)
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode preHead = new ListNode(-1);
+        ListNode curr = preHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = l1 == null ? l2 : l1;
+        return preHead.next;
+
+    }
+
 }
