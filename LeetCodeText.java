55409ce040eba1ecfb4ea48b5841143b4a987ab5
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-08-26 23:00:19 +0800

    89. 格雷编码 (Gray Code)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -18467,4 +18467,19 @@ public class LeetCodeText {
 
     }
 
+    // 89. 格雷编码 (Gray Code)
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        int head = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = res.size() - 1; j >= 0; --j) {
+                res.add(head + res.get(j));
+            }
+            head <<= 1;
+        }
+        return res;
+
+    }
+
 }
