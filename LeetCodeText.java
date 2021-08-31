4025cb78e43d2b0de710ecf9f5017c2166b59e0d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-01 00:24:58 +0800

    1281. 整数的各位积和之差 (Subtract the Product and Sum of Digits of an Integer)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -18781,4 +18781,17 @@ public class LeetCodeText {
         return a;
     }
 
+    // 1281. 整数的各位积和之差 (Subtract the Product and Sum of Digits of an Integer)
+    public int subtractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+        while (n != 0) {
+            int num = n % 10;
+            sum += num;
+            product *= num;
+            n /= 10;
+        }
+        return product - sum;
+    }
+
 }
