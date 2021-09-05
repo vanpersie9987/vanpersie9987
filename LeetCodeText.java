20e3c53a8dd91a39e2a946a7e04ee8c260d5efc2
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-05 21:07:41 +0800

    1925. 统计平方和三元组的数目 (Count Square Sum Triples)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19009,6 +19009,21 @@ public class LeetCodeText {
 
     }
 
+    // 1925. 统计平方和三元组的数目 (Count Square Sum Triples)
+    public int countTriples(int n) {
+        int res = 0;
+        for (int a = 1; a <= n; ++a) {
+            for (int b = 1; b <= n; ++b) {
+                int c = (int) Math.sqrt(a * a + b * b);
+                if (c <= n && a * a + b * b == c * c) {
+                    ++res;
+                }
+            }
+        }
+        return res;
+
+    }
+
     // 491. 递增子序列 (Increasing Subsequences)
     // public List<List<Integer>> findSubsequences(int[] nums) {
     // List<List<Integer>> res = new ArrayList<>();
