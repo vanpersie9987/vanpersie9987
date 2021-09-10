048e364eea442f236e0d6f195a7b9d9fed574ed5
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-10 22:41:55 +0800

    204. 计数质数 (Count Primes)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19293,4 +19293,27 @@ public class LeetCodeText {
         return res;
     }
 
+    // 204. 计数质数 (Count Primes)
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i * i < n; ++i) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime[i]) {
+                ++res;
+            }
+        }
+        return res;
+
+    }
+
+    
+
 }
