792941780d477cf0723ac1ff8deb2f59a7dc37f6
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-11 14:25:33 +0800

    598. 范围求和 II (Range Addition II)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19422,4 +19422,16 @@ public class LeetCodeText {
         return res;
     }
 
+    // 598. 范围求和 II (Range Addition II)
+    public int maxCount(int m, int n, int[][] ops) {
+        int minM = m;
+        int minN = n;
+        for (int[] op : ops) {
+            minM = Math.min(minM, op[0]);
+            minN = Math.min(minN, op[1]);
+        }
+        return minM * minN;
+
+    }
+
 }
