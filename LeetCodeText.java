47a72ccb9e4add8a4b66a38a319df3c615425113
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-12 14:01:25 +0800

    486. 预测赢家 (Predict the Winner)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19478,4 +19478,38 @@ public class LeetCodeText {
 
     }
 
+    // 486. 预测赢家 (Predict the Winner) 二维dp
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+
+    }
+
+    // 486. 预测赢家 (Predict the Winner) 一维dp
+    public boolean PredictTheWinner2(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; ++i) {
+            dp[i] = nums[i];
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+
+        return dp[n - 1] >= 0;
+
+    }
+
 }
