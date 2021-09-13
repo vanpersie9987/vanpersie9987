510a51bb6d34ab4ed6d907d276576fa2aca6ebf0
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-13 21:43:18 +0800

    1952. 三除数 (Three Divisors)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19663,4 +19663,22 @@ public class LeetCodeText {
         return (num + 1) >> 1;
     }
 
+    // 1952. 三除数 (Three Divisors)
+    public boolean isThree(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; ++i) {
+            if (n % i == 0) {
+                ++count;
+                if (i * i != n) {
+                    ++count;
+                }
+                if (count > 3) {
+                    return false;
+                }
+            }
+        }
+        return count == 3;
+
+    }
+
 }
