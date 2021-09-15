683400c63697a28b219944834af9a3f912dfb43e
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-15 21:00:46 +0800

    面试题 16.05. 阶乘尾数 (Factorial Zeros LCCI)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19773,4 +19773,15 @@ public class LeetCodeText {
 
     }
 
+    // 面试题 16.05. 阶乘尾数 (Factorial Zeros LCCI)
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += n / 5;
+            n /= 5;
+        }
+        return res;
+
+    }
+
 }
