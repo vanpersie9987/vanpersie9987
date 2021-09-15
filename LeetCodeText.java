176ccc1d784db4cdee6c7b8570276ded6d666aae
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-15 23:48:44 +0800

    // 264. 丑数 II (Ugly Number II) // 剑指 Offer 49. 丑数

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19775,4 +19775,32 @@ public class LeetCodeText {
 
     }
 
+    // 264. 丑数 II (Ugly Number II)
+    // 剑指 Offer 49. 丑数
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1;
+        int p3 = 1;
+        int p5 = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            int num2 = dp[p2] * 2;
+            int num3 = dp[p3] * 3;
+            int num5 = dp[p5] * 5;
+            dp[i] = Math.min(Math.min(num2, num3), num5);
+            if (dp[i] == num2) {
+                ++p2;
+            }
+            if (dp[i] == num3) {
+                ++p3;
+            }
+            if (dp[i] == num5) {
+                ++p5;
+            }
+        }
+        return dp[n];
+
+    }
+
 }
