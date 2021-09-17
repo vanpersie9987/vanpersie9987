4f39c421259508113e0a5cc8b29dbc38e2fb0e53
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-17 23:51:29 +0800

    1528. 重新排列字符串 (Shuffle String)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19882,4 +19882,14 @@ public class LeetCodeText {
 
     }
 
+    // 1528. 重新排列字符串 (Shuffle String)
+    public String restoreString2(String s, int[] indices) {
+        char[] arr = new char[s.length()];
+        for (int i = 0; i < indices.length; ++i) {
+            arr[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(arr);
+
+    }
+
 }
