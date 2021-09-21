efdab1a0aeecfea11bc3847bce0be84275b257ba
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-21 22:21:12 +0800

    1247. 交换字符使得字符串相同 (Minimum Swaps to Make Strings Equal)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19949,4 +19949,20 @@ public class LeetCodeText {
         return res;
 
     }
+
+    // 1247. 交换字符使得字符串相同 (Minimum Swaps to Make Strings Equal)
+    public int minimumSwap(String s1, String s2) {
+        int countXY = 0;
+        int countYX = 0;
+        for (int i = 0; i < s1.length(); ++i) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (s1.charAt(i) == 'x') {
+                    ++countXY;
+                } else {
+                    ++countYX;
+                }
+            }
+        }
+        return ((countXY + countYX) & 1) == 1 ? -1 : (countXY + 1) / 2 + (countYX + 1) / 2;
+    }
 }
