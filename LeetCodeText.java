a808dd0068c33aba4376134c4c57b5fca353aecf
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-26 20:42:57 +0800

    1929. 数组串联 (Concatenation of Array)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -20193,4 +20193,13 @@ public class LeetCodeText {
 
     }
 
+    // 1929. 数组串联 (Concatenation of Array)
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[nums.length * 2];
+        System.arraycopy(nums, 0, res, 0, nums.length);
+        System.arraycopy(nums, 0, res, nums.length, nums.length);
+        return res;
+
+    }
+
 }
