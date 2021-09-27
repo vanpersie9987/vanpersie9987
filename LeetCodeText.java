9ea23602bcb55717fed24c7e9c36518daeef8d80
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-27 20:35:42 +0800

    1637.两点之间不包含任何点的最宽垂直面积 (Widest Vertical Area Between Two Points Containing No Points)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -20271,4 +20271,16 @@ public class LeetCodeText {
 
     }
 
+    // 1637.两点之间不包含任何点的最宽垂直面积 (Widest Vertical Area Between Two Points Containing No
+    // Points)
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (o1, o2) -> o1[0] - o2[0]);
+        int res = 0;
+        for (int i = 1; i < points.length; ++i) {
+            res = Math.max(res, points[i][0] - points[i - 1][0]);
+        }
+        return res;
+
+    }
+
 }
