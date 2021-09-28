b6b7e29f0505793ae19abb93d1a01b63a3dc1078
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-09-28 22:12:38 +0800

    剑指 Offer II 091. 粉刷房子

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -20391,4 +20391,19 @@ public class LeetCodeText {
         return true;
 
     }
+
+    // 剑指 Offer II 091. 粉刷房子
+    public int minCost(int[][] costs) {
+        int[][] dp = new int[costs.length][3];
+        dp[0][0] = costs[0][0];
+        dp[0][1] = costs[0][1];
+        dp[0][2] = costs[0][2];
+        for (int i = 1; i < costs.length; ++i) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+        }
+        return Math.min(Math.min(dp[costs.length - 1][0], dp[costs.length - 1][1]), dp[costs.length - 1][2]);
+
+    }
 }
