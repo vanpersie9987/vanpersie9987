5866c4c4792a67e7da04cd5b07b36aeb453e39df
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-10-16 19:02:49 +0800

    139. 单词拆分 (Word Break)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -20873,4 +20873,21 @@ public class LeetCodeText {
         return res;
 
     }
+
+    // 139. 单词拆分 (Word Break)
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i < dp.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[dp.length - 1];
+
+    }
 }
