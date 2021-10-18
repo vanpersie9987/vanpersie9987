ab7a6261124e1c61f3e0dfb6c10a407ce15c17a8
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-10-18 23:19:49 +0800

    554. 砖墙 (Brick Wall)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -21032,4 +21032,21 @@ public class LeetCodeText {
             return queue.size();
         }
     }
+
+    // 554. 砖墙 (Brick Wall)
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; ++i) {
+                sum += row.get(i);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        if (map.size() == 0) {
+            return wall.size();
+        }
+        return wall.size() - Collections.max(map.values());
+
+    }
 }
