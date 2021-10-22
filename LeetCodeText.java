b976ba50ce8bfb25be1e395eabe09a2ca6314879
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-10-22 16:35:41 +0800

    21. 合并两个有序链表 (Merge Two Sorted Lists) // 剑指 Offer 25. 合并两个排序的链表

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -18386,21 +18386,22 @@ public class LeetCodeText {
     }
 
     // 21. 合并两个有序链表 (Merge Two Sorted Lists)
+    // 剑指 Offer 25. 合并两个排序的链表
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode preHead = new ListNode(-1);
-        ListNode curr = preHead;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                curr.next = l1;
+                cur.next = l1;
                 l1 = l1.next;
             } else {
-                curr.next = l2;
+                cur.next = l2;
                 l2 = l2.next;
             }
-            curr = curr.next;
+            cur = cur.next;
         }
-        curr.next = l1 == null ? l2 : l1;
-        return preHead.next;
+        cur.next = l1 == null ? l2 : l1;
+        return dummy.next;
 
     }
 
