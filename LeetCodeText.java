55a71ff7fadaf0f8c8c13c5743c5e749171cbfc9
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-10-24 19:00:50 +0800

    24. 两两交换链表中的节点 (Swap Nodes in Pairs)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -21437,4 +21437,20 @@ public class LeetCodeText {
 
     }
 
+    // 24. 两两交换链表中的节点 (Swap Nodes in Pairs)
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode temp = dummy;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummy.next;
+
+    }
+
 }
