20abb1857980709804e04b2105eab3e75e547a38
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-10-26 15:12:33 +0800

    300. 最长递增子序列 (Longest Increasing Subsequence)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -21451,4 +21451,22 @@ public class LeetCodeText {
 
     }
 
+    // 300. 最长递增子序列 (Longest Increasing Subsequence)
+    public int lengthOfLIS(int[] nums) {
+        // dp表示以dp[i]结尾的最长严格递增序列的长度
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 1; i < dp.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+
+    }
+
 }
