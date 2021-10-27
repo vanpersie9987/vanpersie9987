cd5c2d97f6561665e60d0f1fd9f8487cdab7f326
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-10-27 22:23:43 +0800

    1624. 两个相同字符之间的最长子字符串 (Largest Substring Between Two Equal Characters)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -21495,4 +21495,20 @@ public class LeetCodeText {
 
     }
 
+    // 1624. 两个相同字符之间的最长子字符串 (Largest Substring Between Two Equal Characters)
+    public int maxLengthBetweenEqualCharacters(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        int res = -1;
+        for (int i = 0; i < chars.length; ++i) {
+            if (!map.containsKey(chars[i])) {
+                map.put(chars[i], i);
+            } else {
+                res = Math.max(res, i - map.get(chars[i]) - 1);
+            }
+        }
+        return res;
+
+    }
+
 }
