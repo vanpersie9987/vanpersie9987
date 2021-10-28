499bc4beff8d2b05ea8e30606fa0849eb2f45ffc
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-10-28 17:28:17 +0800

    953. 验证外星语词典 (Verifying an Alien Dictionary)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -21547,4 +21547,29 @@ public class LeetCodeText {
         return Arrays.equals(sCounts, tCounts);
     }
 
+    // 953. 验证外星语词典 (Verifying an Alien Dictionary)
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] index = new int[26];
+        for (int i = 0; i < order.length(); ++i) {
+            index[order.charAt(i) - 'a'] = i;
+        }
+        search: for (int i = 0; i < words.length - 1; ++i) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); ++j) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    if (index[word1.charAt(j) - 'a'] > index[word2.charAt(j) - 'a']) {
+                        return false;
+                    }
+                    continue search;
+                }
+            }
+            if (word1.length() > word2.length()) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
 }
