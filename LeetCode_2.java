eff94006dbe356c7f37dd14b6892f5043ac436b2
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-11-14 10:50:03 +0800

    677. 键值映射 (Map Sum Pairs)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -1220,6 +1220,31 @@ public class LeetCode_2 {
       return true;
    }
 
+   // 677. 键值映射 (Map Sum Pairs)
+   class MapSum {
+      private Map<String, Integer> map;
+      private Map<String, Integer> prefixMap;
+
+      public MapSum() {
+         map = new HashMap<>();
+         prefixMap = new HashMap<>();
+
+      }
+
+      public void insert(String key, int val) {
+         int delta = val - map.getOrDefault(key, 0);
+         map.put(key, val);
+         for (int i = 1; i <= key.length(); ++i) {
+            String prefix = key.substring(0, i);
+            prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + delta);
+         }
+      }
+
+      public int sum(String prefix) {
+         return prefixMap.get(prefix);
+      }
+   }
+
    // TODO
    // 523. 连续的子数组和 (Continuous Subarray Sum)
    // public boolean checkSubarraySum(int[] nums, int k) {
