d5c695742f61697381d53282cfb63288c1ae9c66
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-11-19 16:33:47 +0800

    2022. 将一维数组转变成二维数组 (Convert 1D Array Into 2D Array)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -2068,4 +2068,20 @@ public class LeetCode_2 {
 
    }
 
+   // 2022. 将一维数组转变成二维数组 (Convert 1D Array Into 2D Array)
+   public int[][] construct2DArray(int[] original, int m, int n) {
+      if ((m * n) != original.length) {
+         return new int[0][];
+      }
+      int[][] res = new int[m][n];
+      int index = 0;
+      for (int i = 0; i < m; ++i) {
+         for (int j = 0; j < n; ++j) {
+            res[i][j] = original[index++];
+         }
+      }
+      return res;
+
+   }
+
 }
