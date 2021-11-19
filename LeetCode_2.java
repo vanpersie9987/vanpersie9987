df601a1c2ffbbbcaa24fd48828c452d0024f87e3
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-11-19 15:23:42 +0800

    2068. 检查两个字符串是否几乎相等 (Check Whether Two Strings are Almost Equivalent)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -1953,6 +1953,22 @@ public class LeetCode_2 {
 
    }
 
+   // 2068. 检查两个字符串是否几乎相等 (Check Whether Two Strings are Almost Equivalent)
+   public boolean checkAlmostEquivalent(String word1, String word2) {
+      int[] counts = new int[26];
+      for (int i = 0; i < word1.length(); ++i) {
+         ++counts[word1.charAt(i) - 'a'];
+         --counts[word2.charAt(i) - 'a'];
+      }
+      for (int count : counts) {
+         if (Math.abs(count) > 3) {
+            return false;
+         }
+      }
+      return true;
+
+   }
+
    // TODO
    // 523. 连续的子数组和 (Continuous Subarray Sum)
    // public boolean checkSubarraySum(int[] nums, int k) {
