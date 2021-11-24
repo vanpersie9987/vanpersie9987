6d09cbdf7d78e13bf306dbe139bf4a3ac625c75a
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-11-24 23:03:44 +0800

    86. 分隔链表 (Partition List)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -1606,24 +1606,23 @@ public class LeetCode_2 {
    // 86. 分隔链表 (Partition List)
    // 面试题 02.04. 分割链表 (Partition List LCCI)
    public ListNode partition(ListNode head, int x) {
-      ListNode smallDummy = new ListNode(0, head);
-      ListNode small = smallDummy;
-      ListNode largeDummy = new ListNode(0, head);
-      ListNode large = largeDummy;
-      ListNode cur = head;
-      while (cur != null) {
-         if (cur.val < x) {
-            small.next = cur;
+      ListNode small = new ListNode(0, null);
+      ListNode smallHead = small;
+      ListNode large = new ListNode(0, null);
+      ListNode largeHead = large;
+      while (head != null) {
+         if (head.val < x) {
+            small.next = head;
             small = small.next;
          } else {
-            large.next = cur;
+            large.next = head;
             large = large.next;
          }
-         cur = cur.next;
+         head = head.next;
       }
+      small.next = largeHead.next;
       large.next = null;
-      small.next = largeDummy.next;
-      return smallDummy.next;
+      return smallHead.next;
 
    }
 
