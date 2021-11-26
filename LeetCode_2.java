b9b29bc6b685d418938452b1272d794ef5d22012
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-11-26 08:57:47 +0800

    1985. 找出数组中的第 K 大整数 (Find the Kth Largest Integer in the Array)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -2926,4 +2926,21 @@ public class LeetCode_2 {
       list.add(head.val);
    }
 
+   // 1985. 找出数组中的第 K 大整数 (Find the Kth Largest Integer in the Array)
+   public String kthLargestNumber(String[] nums, int k) {
+      Arrays.sort(nums, new Comparator<String>() {
+
+         @Override
+         public int compare(String o1, String o2) {
+            if (o1.length() != o2.length()) {
+               return o2.length() - o1.length();
+            }
+            return o2.compareTo(o1);
+         }
+
+      });
+      return nums[k - 1];
+
+   }
+
 }
