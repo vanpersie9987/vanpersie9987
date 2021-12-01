528002cf6455a9cee65ae427410cd4b22a53c31a
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-12-01 23:08:34 +0800

    1413. 逐步求和得到正数的最小值 (Minimum Value to Get Positive Step by Step Sum)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -3207,4 +3207,15 @@ public class LeetCode_2 {
 
    }
 
+   // 1413. 逐步求和得到正数的最小值 (Minimum Value to Get Positive Step by Step Sum)
+   public int minStartValue(int[] nums) {
+      int min = nums[0];
+      for (int i = 1; i < nums.length; ++i) {
+         nums[i] += nums[i - 1];
+         min = Math.min(min, nums[i]);
+      }
+      return Math.max(1, 1 - min);
+
+   }
+
 }
