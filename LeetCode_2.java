16e4325fd0b59e6ca34fce23c27afcee1aaebc2d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-12-05 11:11:12 +0800

    剑指 Offer II 011. 0 和 1 个数相同的子数组

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -2646,18 +2646,14 @@ public class LeetCode_2 {
    public int findMaxLength(int[] nums) {
       Map<Integer, Integer> map = new HashMap<>();
       map.put(0, -1);
-      int res = 0;
       int count = 0;
+      int res = 0;
       for (int i = 0; i < nums.length; ++i) {
-         if (nums[i] == 1) {
-            ++count;
-         } else {
-            --count;
-         }
-         if (!map.containsKey(count)) {
-            map.put(count, i);
-         } else {
+         count += nums[i] == 0 ? -1 : 1;
+         if (map.containsKey(count)) {
             res = Math.max(res, i - map.get(count));
+         } else {
+            map.put(count, i);
          }
       }
       return res;
