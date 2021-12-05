974a51b8c558d096e8776bb725efbed2e916e1d5
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-12-05 11:01:12 +0800

    剑指 Offer II 013. 二维子矩阵的和

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -2621,16 +2621,19 @@ public class LeetCode_2 {
    // 剑指 Offer II 013. 二维子矩阵的和
    class NumMatrix {
       private int[][] preSum;
+      private int m;
+      private int n;
 
       public NumMatrix(int[][] matrix) {
-         int m = matrix.length;
-         int n = matrix[0].length;
+         m = matrix.length;
+         n = matrix[0].length;
          preSum = new int[m + 1][n + 1];
-         for (int i = 1; i < preSum.length; ++i) {
-            for (int j = 1; j < preSum[0].length; ++j) {
+         for (int i = 1; i < m + 1; ++i) {
+            for (int j = 1; j < n + 1; ++j) {
                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
          }
+
       }
 
       public int sumRegion(int row1, int col1, int row2, int col2) {
