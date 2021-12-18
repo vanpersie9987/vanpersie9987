98167f80a7b1203664830ec09312ed8a5e908537
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-12-18 17:22:49 +0800

    剑指 Offer 47. 礼物的最大价值

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -4603,4 +4603,24 @@ public class LeetCode_2 {
       return res;
 
    }
+
+   // 剑指 Offer 47. 礼物的最大价值
+   public int maxValue(int[][] grid) {
+      int m = grid.length;
+      int n = grid[0].length;
+      for (int i = 0; i < m; ++i) {
+         for (int j = 0; j < n; ++j) {
+            if (i == 0 && j > 0) {
+               grid[i][j] += grid[i][j - 1];
+            } else if (j == 0 && i > 0) {
+               grid[i][j] += grid[i - 1][j];
+            } else if (i > 0 && j > 0) {
+               grid[i][j] += Math.max(grid[i - 1][j], grid[i][j - 1]);
+            }
+         }
+      }
+      return grid[m - 1][n - 1];
+
+   }
+
 }
