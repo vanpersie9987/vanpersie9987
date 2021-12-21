971ac5bd5ba3da1b89be68f327228bff0abce0ba
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-12-21 18:14:55 +0800

    799. 香槟塔 (Champagne Tower)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -4984,4 +4984,21 @@ public class LeetCode_2 {
 
    }
 
+   // 799. 香槟塔 (Champagne Tower)
+   public double champagneTower(int poured, int query_row, int query_glass) {
+      double[][] dp = new double[102][102];
+      dp[0][0] = poured;
+      for (int r = 0; r <= query_row; ++r) {
+         for (int c = 0; c <= r; ++c) {
+            double q = (dp[r][c] - 1.0) / 2;
+            if (q > 0) {
+               dp[r + 1][c] += q;
+               dp[r + 1][c + 1] += q;
+            }
+         }
+      }
+      return Math.min(1.0d, dp[query_row][query_glass]);
+
+   }
+
 }
