2dc1529ac8ff15f47ab8950ae481ecbb011fde1b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2021-12-25 23:10:00 +0800

    419. 甲板上的战舰 (Battleships in a Board)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -5320,4 +5320,26 @@ public class LeetCode_2 {
 
    }
 
+   // 419. 甲板上的战舰 (Battleships in a Board)
+   public int countBattleships(char[][] board) {
+      int m = board.length;
+      int n = board[0].length;
+      int res = 0;
+      for (int i = 0; i < m; ++i) {
+         for (int j = 0; j < n; ++j) {
+            if (board[i][j] == 'X') {
+               if (i > 0 && board[i - 1][j] == 'X') {
+                  continue;
+               }
+               if (j > 0 && board[i][j - 1] == 'X') {
+                  continue;
+               }
+               ++res;
+            }
+         }
+      }
+      return res;
+
+   }
+
 }
