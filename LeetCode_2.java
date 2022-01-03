3df200ba38776020bf9627793bcb1ea31fc52473
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-01-03 14:59:00 +0800

    225. 用队列实现栈 (Implement Stack using Queues) --两个队列

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -5813,4 +5813,39 @@ public class LeetCode_2 {
 
    }
 
+   // 225. 用队列实现栈 (Implement Stack using Queues) --两个队列
+   class MyStack {
+      Queue<Integer> queue1;
+      Queue<Integer> queue2;
+
+      public MyStack() {
+         queue1 = new LinkedList<>();
+         queue2 = new LinkedList<>();
+      }
+
+      public void push(int x) {
+         queue2.offer(x);
+         while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+         }
+         Queue<Integer> temp = queue2;
+         queue2 = queue1;
+         queue1 = temp;
+      }
+
+      public int pop() {
+         return queue1.poll();
+
+      }
+
+      public int top() {
+         return queue1.peek();
+
+      }
+
+      public boolean empty() {
+         return queue1.isEmpty();
+      }
+   }
+
 }
