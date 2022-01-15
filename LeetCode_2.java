3cbbf1a9172617f1e590884e0100a6aa385e5396
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-01-15 10:36:00 +0800

    859. 亲密字符串 (Buddy Strings)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -6867,4 +6867,37 @@ public class LeetCode_2 {
 
    }
 
+   // 859. 亲密字符串 (Buddy Strings)
+   public boolean buddyStrings(String s, String goal) {
+      if (s.length() != goal.length()) {
+         return false;
+      }
+      if (s.equals(goal)) {
+         int[] counts = new int[26];
+         for (char c : s.toCharArray()) {
+            if (++counts[c - 'a'] > 1) {
+               return true;
+            }
+         }
+         return false;
+      }
+      int firstIndex = -1;
+      int secondIndex = -1;
+      for (int i = 0; i < s.length(); ++i) {
+         if (s.charAt(i) != goal.charAt(i)) {
+            if (firstIndex == -1) {
+               firstIndex = i;
+            } else if (secondIndex == -1) {
+               secondIndex = i;
+            } else {
+               return false;
+            }
+
+         }
+      }
+      return secondIndex != -1 && s.charAt(firstIndex) == goal.charAt(secondIndex)
+            && s.charAt(secondIndex) == goal.charAt(firstIndex);
+
+   }
+
 }
