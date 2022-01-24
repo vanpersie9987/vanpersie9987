b35ccc1572ac6a79c51af93df6822cdeebe6f0fc
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-01-24 23:40:13 +0800

    406. 根据身高重建队列 (Queue Reconstruction by Height)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -7389,4 +7389,25 @@ public class LeetCode_2 {
 
    }
 
+   // 406. 根据身高重建队列 (Queue Reconstruction by Height)
+   public int[][] reconstructQueue(int[][] people) {
+      Arrays.sort(people, new Comparator<int[]>() {
+
+         @Override
+         public int compare(int[] o1, int[] o2) {
+            if (o1[0] == o2[0]) {
+               return o1[1] - o2[1];
+            }
+            return o2[0] - o1[0];
+
+         }
+      });
+      List<int[]> res = new ArrayList<>();
+      for (int[] p : people) {
+         res.add(p[1], p);
+      }
+      return res.toArray(new int[res.size()][]);
+
+   }
+
 }
