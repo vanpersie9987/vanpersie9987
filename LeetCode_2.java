a5051e13767024bde087efe4518b1c5544a4c300
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-01-27 00:22:16 +0800

    1846. 减小和重新排列数组后的最大元素 (Maximum Element After Decreasing and Rearranging)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -7518,4 +7518,15 @@ public class LeetCode_2 {
       return Arrays.equals(expected, target);
    }
 
+   // 1846. 减小和重新排列数组后的最大元素 (Maximum Element After Decreasing and Rearranging)
+   public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+      Arrays.sort(arr);
+      arr[0] = 1;
+      for (int i = 1; i < arr.length; ++i) {
+         arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+      }
+      return arr[arr.length - 1];
+
+   }
+
 }
