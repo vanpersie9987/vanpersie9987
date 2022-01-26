bf869dfade098962b40ed42accc2a032d634ad6f
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-01-26 12:54:30 +0800

    1029. 两地调度 (Two City Scheduling)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -7459,4 +7459,21 @@ public class LeetCode_2 {
 
    }
 
+   // 1029. 两地调度 (Two City Scheduling)
+   public int twoCitySchedCost(int[][] costs) {
+      Arrays.sort(costs, new Comparator<int[]>() {
+
+         @Override
+         public int compare(int[] o1, int[] o2) {
+            return o1[0] - o1[1] - (o2[0] - o2[1]);
+         }
+      });
+      int res = 0;
+      for (int i = 0; i < costs.length / 2; ++i) {
+         res += costs[i][0] + costs[i + costs.length / 2][1];
+      }
+      return res;
+
+   }
+
 }
