8c4c29dfd061f89132e615d00e938b9939fb6302
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-01-27 21:49:43 +0800

    1727. 重新排列后的最大子矩阵 (Largest Submatrix With Rearrangements)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -7567,4 +7567,26 @@ public class LeetCode_2 {
 
    }
 
+   // 1727. 重新排列后的最大子矩阵 (Largest Submatrix With Rearrangements)
+   public int largestSubmatrix(int[][] matrix) {
+      int m = matrix.length;
+      int n = matrix[0].length;
+      int res = 0;
+      for (int i = 1; i < m; ++i) {
+         for (int j = 0; j < n; ++j) {
+            if (matrix[i][j] == 1) {
+               matrix[i][j] += matrix[i - 1][j];
+            }
+         }
+      }
+      for (int i = 0; i < m; ++i) {
+         Arrays.sort(matrix[i]);
+         for (int j = n - 1; j >= 0; --j) {
+            res = Math.max(res, matrix[i][j] * (n - j));
+         }
+      }
+      return res;
+
+   }
+
 }
