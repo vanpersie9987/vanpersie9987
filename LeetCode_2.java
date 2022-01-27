e5061546645c228df5d4630ab2af9497e3da22d1
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-01-27 23:18:59 +0800

    646. 最长数对链 (Maximum Length of Pair Chain)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -7589,4 +7589,25 @@ public class LeetCode_2 {
 
    }
 
+   // 646. 最长数对链 (Maximum Length of Pair Chain)
+   public int findLongestChain(int[][] pairs) {
+      Arrays.sort(pairs, new Comparator<int[]>() {
+
+         @Override
+         public int compare(int[] o1, int[] o2) {
+            return o1[1] - o2[1];
+         }
+      });
+      int pre = Integer.MIN_VALUE;
+      int res = 0;
+      for (int[] pair : pairs) {
+         if (pre < pair[0]) {
+            ++res;
+            pre = pair[1];
+         }
+      }
+      return res;
+
+   }
+
 }
