b748aba292ae7a977bbbb03be35416e31f234388
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-02-03 13:04:14 +0800

    700. 二叉搜索树中的搜索 (Search in a Binary Search Tree) --迭代

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -8373,4 +8373,18 @@ public class LeetCode_2 {
       return node;
    }
 
+   // 700. 二叉搜索树中的搜索 (Search in a Binary Search Tree) --迭代
+   public TreeNode searchBST(TreeNode root, int val) {
+      while (root != null) {
+         if (root.val == val) {
+            return root;
+         } else if (root.val > val) {
+            root = root.left;
+         } else if (root.val < val) {
+            root = root.right;
+         }
+      }
+      return null;
+   }
+
 }
