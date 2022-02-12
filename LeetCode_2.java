aa0d37c2b0767a27c7b97ae4efcde0700f717370
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-02-12 13:39:29 +0800

    96. 不同的二叉搜索树 (Unique Binary Search Trees)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -9761,4 +9761,18 @@ public class LeetCode_2 {
       return root;
    }
 
+   // 96. 不同的二叉搜索树 (Unique Binary Search Trees)
+   public int numTrees(int n) {
+      int[] G = new int[n + 1];
+      G[0] = 1;
+      G[1] = 1;
+      for (int i = 2; i <= n; ++i) {
+         for (int j = 1; j <= i; ++j) {
+            G[i] += G[j - 1] * G[i - j];
+         }
+      }
+      return G[n];
+
+   }
+
 }
