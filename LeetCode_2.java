03fa84424f1282f3e0b2d7f02ccc4d2013de3c7c
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-02-14 13:34:12 +0800

    540. 有序数组中的单一元素 (Single Element in a Sorted Array)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -194,13 +194,30 @@ public class LeetCode_2 {
    }
 
    // 540. 有序数组中的单一元素 (Single Element in a Sorted Array)
-   // 剑指 Offer II 070. 排序数组中只出现一次的数字 还需要掌握二分查找法
+   // 剑指 Offer II 070. 排序数组中只出现一次的数字
    public int singleNonDuplicate(int[] nums) {
       int res = 0;
       for (int num : nums) {
          res ^= num;
       }
       return res;
+
+   }
+
+   // 540. 有序数组中的单一元素 (Single Element in a Sorted Array)
+   // 剑指 Offer II 070. 排序数组中只出现一次的数字 --二分查找
+   public int singleNonDuplicate2(int[] nums) {
+      int left = 0;
+      int right = nums.length - 1;
+      while (left < right) {
+         int mid = left + ((right - left) >>> 1);
+         if (nums[mid] == nums[mid ^ 1]) {
+            left = mid + 1;
+         } else {
+            right = mid;
+         }
+      }
+      return nums[left];
 
    }
 
