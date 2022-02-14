c4c0e4a8654707b6c5c765a361c851689ac686bf
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-02-14 14:20:22 +0800

    100. 相同的树 (Same Tree) --bfs

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -9843,12 +9843,34 @@ public class LeetCode_2 {
       return res;
    }
 
-   // 100. 相同的树 (Same Tree) --bfs
+   // 100. 相同的树 (Same Tree) --dfs
    public boolean isSameTree(TreeNode p, TreeNode q) {
       if (p == null) {
          return q == null;
       }
       return q != null && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
+   }
+
+   // 100. 相同的树 (Same Tree) --bfs
+   public boolean isSameTree2(TreeNode p, TreeNode q) {
+      Queue<TreeNode> queue = new LinkedList<>();
+      queue.offer(p);
+      queue.offer(q);
+      while (!queue.isEmpty()) {
+         TreeNode node1 = queue.poll();
+         TreeNode node2 = queue.poll();
+         if (node1 == null && node2 == null) {
+            continue;
+         }
+         if (node1 == null || node2 == null || node1.val != node2.val) {
+            return false;
+         }
+         queue.offer(node1.left);
+         queue.offer(node2.left);
+         queue.offer(node1.right);
+         queue.offer(node2.right);
+      }
+      return queue.isEmpty();
    }
 }
