0083b798ee9e270f6f4b1fb7324c81c07026e6a2
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-02-14 14:26:44 +0800

    102. 二叉树的层序遍历 (Binary Tree Level Order Traversal)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -9873,4 +9873,31 @@ public class LeetCode_2 {
       }
       return queue.isEmpty();
    }
+
+   // 102. 二叉树的层序遍历 (Binary Tree Level Order Traversal)
+   public List<List<Integer>> levelOrder(TreeNode root) {
+      List<List<Integer>> res = new ArrayList<>();
+      if (root == null) {
+         return res;
+      }
+      Queue<TreeNode> queue = new LinkedList<>();
+      queue.offer(root);
+      while (!queue.isEmpty()) {
+         int size = queue.size();
+         List<Integer> list = new ArrayList<>();
+         for (int i = 0; i < size; ++i) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) {
+               queue.offer(node.left);
+            }
+            if (node.right != null) {
+               queue.offer(node.right);
+            }
+         }
+         res.add(list);
+      }
+      return res;
+
+   }
 }
