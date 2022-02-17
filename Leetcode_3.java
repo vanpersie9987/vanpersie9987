6d371ed5fbb6dc1133c507b1c006787e3b635824
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-02-17 17:04:49 +0800

    23. 合并K个升序链表 (Merge k Sorted Lists) --分治

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -253,4 +253,36 @@ public class Leetcode_3 {
         return dummy.next;
     }
 
+    // 23. 合并K个升序链表 (Merge k Sorted Lists) --分治
+    public ListNode mergeKLists2(ListNode[] lists) {
+        return getLists(lists, 0, lists.length - 1);
+    }
+
+    private ListNode getLists(ListNode[] lists, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        if (left == right) {
+            return lists[left];
+        }
+        int mid = left + ((right - left) >>> 1);
+        return merge(getLists(lists, left, mid), getLists(lists, mid + 1, right));
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                cur.next = head1;
+                head1 = head1.next;
+            } else {
+                cur.next = head2;
+                head2 = head2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = head1 != null ? head1 : head2;
+        return dummy.next;
+    }
 }
