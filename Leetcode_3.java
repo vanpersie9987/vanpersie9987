8d6a450b817ac1692e23aa464b03fb4f6568572b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-02-19 09:31:26 +0800

    2100. 适合打劫银行的日子 (Find Good Days to Rob the Bank)

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -427,4 +427,29 @@ public class Leetcode_3 {
 
     }
 
+    // 2100. 适合打劫银行的日子 (Find Good Days to Rob the Bank)
+    public List<Integer> goodDaysToRobBank(int[] security, int time) {
+        int n = security.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for (int i = 1; i < n; ++i) {
+            if (security[i - 1] >= security[i]) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            if (security[i] <= security[i + 1]) {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            if (left[i] >= time && right[i] >= time) {
+                res.add(i);
+            }
+        }
+        return res;
+
+    }
+
 }
