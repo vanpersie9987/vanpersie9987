cda542c8870d956971416bc55dede4692733c85d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-03-04 11:16:37 +0800

    243. 最短单词距离 (Shortest Word Distance) --plus

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -2309,7 +2309,7 @@ public class Leetcode_3 {
 
     }
 
-    // 531. 孤独像素 I (Lonely Pixel I)
+    // 531. 孤独像素 I (Lonely Pixel I) --plus
     public int findLonelyPixel(char[][] picture) {
         int m = picture.length;
         int n = picture[0].length;
@@ -2332,6 +2332,24 @@ public class Leetcode_3 {
             }
         }
         return res;
+
+    }
+
+    // 243. 最短单词距离 (Shortest Word Distance) --plus
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int i1 = -1, i2 = -1;
+        int minDistance = wordsDict.length;
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                i1 = i;
+            } else if (wordsDict[i].equals(word2)) {
+                i2 = i;
+            }
+            if (i1 != -1 && i2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(i1 - i2));
+            }
+        }
+        return minDistance;
 
     }
     // 814. 二叉树剪枝 (Binary Tree Pruning) --后序遍历
