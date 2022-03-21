2664c85b038256a331563fabb539771d30add34e
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-03-21 21:10:00 +0800

    2210. 统计数组中峰和谷的数量 (Count Hills and Valleys in an Array)

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -4069,44 +4069,45 @@ public class Leetcode_3 {
         return res;
 
     }
-    // // 6027. 统计数组中峰和谷的数量
-    // public int countHillValley(int[] nums) {
-    // int res = 0;
-    // for (int i = 1; i < nums.length - 1; ++i) {
-    // if (nums[i] == nums[i - 1]) {
-    // continue;
-    // }
-    // if (isPeek6027(i, i - 1, i + 1, nums)) {
-    // ++res;
-    // }
-    // }
-    // return res;
 
-    // }
+    // 2210. 统计数组中峰和谷的数量 (Count Hills and Valleys in an Array)
+    public int countHillValley(int[] nums) {
+        int res = 0;
+        for (int i = 1; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (isPeek6027(i, nums)) {
+                ++res;
+            }
+        }
+        return res;
 
-    // private boolean isPeek6027(int index, int left, int right, int[] nums) {
-    // while (left >= 0 && right < nums.length) {
-    // if (nums[index] == nums[left]) {
-    // --left;
-    // continue;
-    // }
-    // if (nums[index] == nums[right]) {
-    // ++right;
-    // continue;
-    // }
-    // if (nums[left] > nums[index] && nums[index] > nums[right]) {
-    // return false;
-    // } else if (nums[left] < nums[index] && nums[index] < nums[right]) {
-    // return false;
-    // }
-    // if ((nums[index] > nums[left] && nums[index] > nums[right]) || (nums[index] <
-    // nums[left]
-    // && nums[index] < nums[right])) {
-    // return true;
-    // }
-    // }
-    // return false;
-    // }
+    }
+
+    private boolean isPeek6027(int index, int[] nums) {
+        int left = index - 1;
+        int right = index + 1;
+        while (left >= 0 && right < nums.length) {
+            if (nums[index] == nums[left]) {
+                --left;
+                continue;
+            }
+            if (nums[index] == nums[right]) {
+                ++right;
+                continue;
+            }
+            if ((nums[left] > nums[index] && nums[index] > nums[right])
+                    || (nums[left] < nums[index] && nums[index] < nums[right])) {
+                return false;
+            }
+            if ((nums[index] > nums[left] && nums[index] > nums[right]) || (nums[index] < nums[left]
+                    && nums[index] < nums[right])) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     // // 6028. 统计道路上的碰撞次数
     // public int countCollisions(String directions) {
