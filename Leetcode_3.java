2a6bf9e0a5c1fb85a0445fcfa41e7c0ae9086561
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-03-22 00:41:10 +0800

    优化

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -4115,47 +4115,6 @@ public class Leetcode_3 {
         return res;
     }
 
-    // 2039. 网络空闲的时刻 (The Time When the Network Becomes Idle)
-    // public int networkBecomesIdle(int[][] edges, int[] patience) {
-
-    // }
-
-    // 2157. 字符串分组 (Groups of Strings)
-    // public int[] groupStrings(String[] words) {
-
-    // }
-
-    // 529. 扫雷游戏 (Minesweeper)
-    // public char[][] updateBoard(char[][] board, int[] click) {
-
-    // }
-
-    // 1443. 收集树上所有苹果的最少时间 (Minimum Time to Collect All Apples in a Tree)
-    // public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
-
-    // }
-
-    // 617. 合并二叉树 (Merge Two Binary Trees)
-    // public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-
-    // }
-
-    // 787. K 站中转内最便宜的航班 (Cheapest Flights Within K Stops)
-    // public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k)
-    // {
-
-    // }
-
-    // 1376. 通知所有员工所需的时间 (Time Needed to Inform All Employees)
-    // public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-
-    // }
-
-    // 1466. 重新规划路线 (Reorder Routes to Make All Paths Lead to the City Zero)
-    // public int minReorder(int n, int[][] connections) {
-
-    // }
-
     // 207. 课程表 (Course Schedule) --bfs + 拓扑排序
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, Integer> inDegrees = new HashMap<>();
@@ -4222,6 +4181,47 @@ public class Leetcode_3 {
         return count == numCourses ? res : new int[0];
 
     }
+
+    // 2039. 网络空闲的时刻 (The Time When the Network Becomes Idle)
+    // public int networkBecomesIdle(int[][] edges, int[] patience) {
+
+    // }
+
+    // 2157. 字符串分组 (Groups of Strings)
+    // public int[] groupStrings(String[] words) {
+
+    // }
+
+    // 529. 扫雷游戏 (Minesweeper)
+    // public char[][] updateBoard(char[][] board, int[] click) {
+
+    // }
+
+    // 1443. 收集树上所有苹果的最少时间 (Minimum Time to Collect All Apples in a Tree)
+    // public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+
+    // }
+
+    // 617. 合并二叉树 (Merge Two Binary Trees)
+    // public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+
+    // }
+
+    // 787. K 站中转内最便宜的航班 (Cheapest Flights Within K Stops)
+    // public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+    // {
+
+    // }
+
+    // 1376. 通知所有员工所需的时间 (Time Needed to Inform All Employees)
+    // public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+
+    // }
+
+    // 1466. 重新规划路线 (Reorder Routes to Make All Paths Lead to the City Zero)
+    // public int minReorder(int n, int[][] connections) {
+
+    // }
 
     // 647. 回文子串 (Palindromic Substrings)
     // 剑指 Offer II 020. 回文子字符串的个数
