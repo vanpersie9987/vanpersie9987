e21a60ccefdb43760a51ab322f00edc3b37e51cc
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-04-06 22:21:55 +0800

    623. 在二叉树中增加一行 (Add One Row to Tree) --bfs

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -5798,6 +5798,45 @@ public class Leetcode_3 {
 
     }
 
+    // 623. 在二叉树中增加一行 (Add One Row to Tree) --bfs
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            TreeNode res = new TreeNode(val, root, null);
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            ++level;
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = queue.poll();
+                if (level == depth - 1) {
+                    TreeNode tempLeft = node.left;
+                    node.left = new TreeNode(val);
+                    node.left.left = tempLeft;
+
+                    TreeNode tempRight = node.right;
+                    node.right = new TreeNode(val);
+                    node.right.right = tempRight;
+                } else {
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+            }
+            if (level == depth - 1) {
+                break;
+            }
+        }
+        return root;
+
+    }
+
     // LCP 41. 黑白翻转棋
     // public int flipChess(String[] chessboard) {
 
