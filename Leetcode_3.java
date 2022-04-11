589949b5b4cb9be582c17f664398e31b3d74488b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-04-11 22:38:33 +0800

    617. 合并二叉树 (Merge Two Binary Trees) --bfs

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -6355,4 +6355,53 @@ public class Leetcode_3 {
             return root;
         }
     }
+
+    // 617. 合并二叉树 (Merge Two Binary Trees) --bfs
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        queue.offer(root);
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        queue1.offer(root1);
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        queue2.offer(root2);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode node1 = queue1.poll();
+            TreeNode node2 = queue2.poll();
+            if (node1.left != null || node2.left != null) {
+                if (node1.left != null && node2.left != null) {
+                    node.left = new TreeNode(node1.left.val + node2.left.val);
+                    queue.offer(node.left);
+                    queue1.offer(node1.left);
+                    queue2.offer(node2.left);
+                } else if (node1.left != null) {
+                    node.left = node1.left;
+                } else if (node2.left != null) {
+                    node.left = node2.left;
+                }
+            }
+
+            if (node1.right != null || node2.right != null) {
+                if (node1.right != null && node2.right != null) {
+                    node.right = new TreeNode(node1.right.val + node2.right.val);
+                    queue.offer(node.right);
+                    queue1.offer(node1.right);
+                    queue2.offer(node2.right);
+                } else if (node1.right != null) {
+                    node.right = node1.right;
+                } else if (node2.right != null) {
+                    node.right = node2.right;
+                }
+            }
+        }
+        return root;
+
+    }
 }
