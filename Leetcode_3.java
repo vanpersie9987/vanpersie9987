1b5ee842f5949cfdfaddf8dfa43fc6b9bb2db21a
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-04-13 11:59:42 +0800

    284. 顶端迭代器 (Peeking Iterator)

--- Leetcode_3.java
+++ Leetcode_3.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -6518,6 +6519,40 @@ public class Leetcode_3 {
             bit >>= 1;
         }
         return node != null;
+    }
+
+    // 284. 顶端迭代器 (Peeking Iterator)
+    class PeekingIterator implements Iterator<Integer> {
+        private Iterator<Integer> iterator;
+        private Integer nextInteger;
+
+        public PeekingIterator(Iterator<Integer> iterator) {
+            // initialize any member here.
+            this.iterator = iterator;
+            this.nextInteger = iterator.next();
+
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        public Integer peek() {
+            return nextInteger;
+
+        }
+
+        // hasNext() and next() should behave the same as in the Iterator interface.
+        // Override them if needed.
+        @Override
+        public Integer next() {
+            Integer res = nextInteger;
+            nextInteger = iterator.hasNext() ? iterator.next() : null;
+            return res;
+
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextInteger != null;
+        }
     }
 
     // 310. 最小高度树 (Minimum Height Trees)
