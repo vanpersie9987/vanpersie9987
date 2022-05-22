e40ccfcad08afb59ff4ad079908c767747a56f0c
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-05-22 12:28:32 +0800

    294-周赛

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -1,6 +1,132 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class LeetCode_4 {
     public static void main(String[] args) {
 
+    }
+
+    // 6074. 字母在字符串中的百分比
+    public int percentageLetter(String s, char letter) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == letter) {
+                ++count;
+            }
+        }
+        double percentage = (double) count / s.length();
+        return (int) (percentage * 100);
+
+    }
+
+    // 6075. 装满石头的背包的最大数量
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        int res = 0;
+        Queue<Integer> queue = new PriorityQueue<>();
+        int n = rocks.length;
+        for (int i = 0; i < n; ++i) {
+            queue.offer(capacity[i] - rocks[i]);
+        }
+        while (!queue.isEmpty() && queue.peek() <= additionalRocks) {
+            ++res;
+            int cur = queue.poll();
+            additionalRocks -= cur;
+        }
+        return res;
+
+    }
+
+    // 6076. 表示一个折线图的最少线段数
+    public int minimumLines(int[][] stockPrices) {
+        int n = stockPrices.length;
+        if (n == 1) {
+            return 0;
+        }
+        Arrays.sort(stockPrices, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+
+        });
+
+        int res = 1;
+        // 分子
+        int den = stockPrices[1][1] - stockPrices[0][1];
+        // 分母
+        int num = stockPrices[1][0] - stockPrices[0][0];
+        // 符号 0 == 0 1 == 正号 2 == 负号
+        int sign = 0;
+        if (den == 0) {
+            sign = 0;
+        } else if ((den > 0 && num > 0) || (den < 0 && num < 0)) {
+            sign = 1;
+        } else {
+            sign = 2;
+        }
+        den = Math.abs(den);
+        num = Math.abs(num);
+        int gcd = getGCD(den, num);
+        den /= gcd;
+        num /= gcd;
+        for (int i = 2; i < stockPrices.length; ++i) {
+            // 分子
+            int curDen = stockPrices[i][1] - stockPrices[i - 1][1];
+            // 分母
+            int curNum = stockPrices[i][0] - stockPrices[i - 1][0];
+            int curSign = 0;
+
+            if (curDen == 0) {
+                curSign = 0;
+            } else if ((curDen > 0 && curNum > 0) || (curDen < 0 && curNum < 0)) {
+                curSign = 1;
+            } else {
+                curSign = 2;
+            }
+
+            curDen = Math.abs(curDen);
+            curNum = Math.abs(curNum);
+            int curGcd = getGCD(curDen, curNum);
+            curDen /= curGcd;
+            curNum /= curGcd;
+            if (!((curDen == 0 && den == 0) || (curSign == sign && curDen == den && curNum == num))) {
+                ++res;
+            }
+            sign = curSign;
+            den = curDen;
+            num = curNum;
+
+        }
+        return res;
+
+    }
+
+    // 计算最大公约数
+    private int getGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
 }
