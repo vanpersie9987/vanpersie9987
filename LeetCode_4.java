a4cec0cc6454b688caab355adf5f3293e51aff8a
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-05-31 22:32:52 +0800

    208. 实现 Trie (前缀树) (Implement Trie (Prefix Tree))

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -784,4 +784,50 @@ public class LeetCode_4 {
         return dp[m - 1][n - 1];
 
     }
+
+    // 208. 实现 Trie (前缀树) (Implement Trie (Prefix Tree))
+    class Trie {
+        private Trie[] children;
+        private boolean isEnd;
+
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
+        }
+
+        public void insert(String word) {
+            Trie node = this;
+            for (char c : word.toCharArray()) {
+                int index = c - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
+            }
+            node.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            Trie node = searchPrefix(word);
+            return node != null && node.isEnd;
+
+        }
+
+        private Trie searchPrefix(String word) {
+            Trie node = this;
+            for (char c : word.toCharArray()) {
+                int index = c - 'a';
+                if (node.children[index] == null) {
+                    return null;
+                }
+                node = node.children[index];
+            }
+            return node;
+        }
+
+        public boolean startsWith(String prefix) {
+            Trie node = searchPrefix(prefix);
+            return node != null;
+        }
+    }
 }
