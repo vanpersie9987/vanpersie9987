a3d109398180cf345a24c60cfd609719753c07a2
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-06-27 20:55:23 +0800

    1795. 每个产品在不同商店的价格 (Rearrange Products Table)

--- leetcode_sql.sql
+++ leetcode_sql.sql
@@ -139,8 +139,27 @@ HAVING
     count(*) >= 3
 ;
 
+-- 1795. 每个产品在不同商店的价格 (Rearrange Products Table)
+SELECT 
+    product_id , 'store1' AS store , store1 AS price
+FROM
+    Products
+WHERE store1 IS NOT NULL
 
+UNION ALL
 
+SELECT 
+    product_id , 'store2' AS store , store2 AS price
+FROM
+    Products
+WHERE store2 IS NOT NULL
 
+UNION ALL
 
+SELECT 
+    product_id , 'store3' AS store , store3 AS price
+FROM
+    Products
+WHERE store3 IS NOT NULL
+;
 
