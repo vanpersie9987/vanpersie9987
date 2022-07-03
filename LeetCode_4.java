5cfa38cf8ed01226ecc1d91cd3d32cb04639274f
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-03 18:10:07 +0800

    1143. 最长公共子序列 (Longest Common Subsequence)

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -3836,4 +3836,48 @@ public class LeetCode_4 {
         return dp[i][j] = res;
     }
 
+    // 1143. 最长公共子序列 (Longest Common Subsequence) --二维dp
+    // 剑指 Offer II 095. 最长公共子序列
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i < m + 1; ++i) {
+            char c1 = text1.charAt(i - 1);
+            for (int j = 1; j < n + 1; ++j) {
+                char c2 = text2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    // 1143. 最长公共子序列 (Longest Common Subsequence) --一维dp
+    // 剑指 Offer II 095. 最长公共子序列
+    public int longestCommonSubsequence2(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[2][n + 1];
+        for (int i = 1; i < m + 1; ++i) {
+            char c1 = text1.charAt(i - 1);
+            for (int j = 1; j < n + 1; ++j) {
+                char c2 = text2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[1][j] = dp[0][j - 1] + 1;
+                } else {
+                    dp[1][j] = Math.max(dp[1][j - 1], dp[0][j]);
+                }
+            }
+            for (int j = 1; j < n + 1; ++j) {
+                dp[0][j] = dp[1][j];
+                dp[1][j] = 0;
+            }
+        }
+        return dp[0][n];
+    }
+
 }
