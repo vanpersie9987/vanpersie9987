9821c73e8060f4c4b6e88476698c452cddfd4403
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-09 12:21:04 +0800

    1736. Latest Time by Replacing Hidden Digits

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -4803,4 +4803,35 @@ public class LeetCode_4 {
 
     }
 
+    // 1736. Latest Time by Replacing Hidden Digits
+    public String maximumTime(String time) {
+        char[] chars = time.toCharArray();
+        // 小时
+        if (chars[0] == '?' && chars[1] == '?') {
+            chars[0] = '2';
+            chars[1] = '3';
+        } else if (chars[0] == '?') {
+            if (chars[1] >= '0' && chars[1] <= '3') {
+                chars[0] = '2';
+            } else {
+                chars[0] = '1';
+            }
+        } else if (chars[1] == '?') {
+            if (chars[0] == '0' || chars[0] == '1') {
+                chars[1] = '9';
+            } else {
+                chars[1] = '3';
+            }
+        }
+        // 分钟
+        if (chars[3] == '?') {
+            chars[3] = '5';
+        }
+        if (chars[4] == '?') {
+            chars[4] = '9';
+        }
+        return String.valueOf(chars);
+
+    }
+
 }
