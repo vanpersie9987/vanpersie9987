f3716236e594fd0cea58d450745be969525ed398
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-11 21:03:51 +0800

    589. N 叉树的前序遍历 (N-ary Tree Preorder Traversal)

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -5032,4 +5032,58 @@ public class LeetCode_4 {
 
     }
 
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    // 589. N 叉树的前序遍历 (N-ary Tree Preorder Traversal)
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs589(root, res);
+        return res;
+
+    }
+
+    private void dfs589(Node root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        for (Node child : root.children) {
+            dfs589(child, res);
+        }
+    }
+
+    // 589. N 叉树的前序遍历 (N-ary Tree Preorder Traversal) --迭代
+    public List<Integer> preorder2(Node root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            res.add(node.val);
+            for (int i = node.children.size() - 1; i >= 0; --i) {
+                stack.push(node.children.get(i));
+            }
+        }
+        return res;
+
+    }
+
 }
