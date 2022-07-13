6612b4f86a28e194c25dc9773935ed6185f7f637
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-13 22:49:02 +0800

    712. 两个字符串的最小ASCII删除和 (Minimum ASCII Delete Sum for Two Strings) --dp

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -5319,6 +5319,30 @@ public class LeetCode_4 {
 
     }
 
+    // 712. 两个字符串的最小ASCII删除和 (Minimum ASCII Delete Sum for Two Strings) --dp
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = m - 1; i >= 0; --i) {
+            dp[i][n] = dp[i + 1][n] + s1.codePointAt(i);
+        }
+        for (int j = n - 1; j >= 0; --j) {
+            dp[m][j] = dp[m][j + 1] + s2.codePointAt(j);
+        }
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j] + s1.codePointAt(i), dp[i][j + 1] + s2.codePointAt(j));
+                }
+            }
+        }
+        return dp[0][0];
+
+    }
+
     // 979. 在二叉树中分配硬币 (Distribute Coins in Binary Tree)
     // public int distributeCoins(TreeNode root) {
 
@@ -5329,8 +5353,13 @@ public class LeetCode_4 {
 
     // }
 
-    // 712. 两个字符串的最小ASCII删除和 (Minimum ASCII Delete Sum for Two Strings)
-    // public int minimumDeleteSum(String s1, String s2) {
+    // 390. 消除游戏 (Elimination Game)
+    // public int lastRemaining(int n) {
+
+    // }
+
+    // 393. UTF-8 编码验证 (UTF-8 Validation)
+    // public boolean validUtf8(int[] data) {
 
     // }
 }
