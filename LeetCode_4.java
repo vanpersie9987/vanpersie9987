eb9b1f98716ab0420cb1301302a5e997042236b9
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-17 13:28:53 +0800

    面试题 17.01. 不用加号的加法

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -5771,14 +5771,14 @@ public class LeetCode_4 {
 
     // 面试题 17.01. 不用加号的加法
     // 剑指 Offer 65. 不用加减乘除做加法
-    // public int add(int a, int b) {
-    //     while (b != 0) {
-    //         int c = (a & b) << 1;
-    //         a ^= b;
-    //         b = c;
-    //     }
-    //     return a;
-    // }
+    public int add(int a, int b) {
+        while (b != 0) {
+            int c = (a & b) << 1;
+            a ^= b;
+            b = c;
+        }
+        return a;
+    }
 
     // 979. 在二叉树中分配硬币 (Distribute Coins in Binary Tree)
     // public int distributeCoins(TreeNode root) {
