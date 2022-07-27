2c12075c703dbf7aa30654166cc50c5bc8eaa906
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-27 21:24:37 +0800

    664. 奇怪的打印机 (Strange Printer) --dp

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -6563,6 +6563,33 @@ public class LeetCode_4 {
         return (x << 16) | y;
     }
 
+    // 664. 奇怪的打印机 (Strange Printer) --dp
+    public int strangePrinter(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; ++j) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    int min = Integer.MAX_VALUE;
+                    for (int k = i; k < j; ++k) {
+                        min = Math.min(min, dp[i][k] + dp[k + 1][j]);
+                    }
+                    dp[i][j] = min;
+                }
+            }
+        }
+        return dp[0][n - 1];
+
+    }
+
+    // 526. 优美的排列 (Beautiful Arrangement)
+    // public int countArrangement(int n) {
+
+    // }
+
     // 1049. 最后一块石头的重量 II (Last Stone Weight II)
     // public int lastStoneWeightII(int[] stones) {
 
