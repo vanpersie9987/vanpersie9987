4dd382770094fbe81204851b54b7945cb8d00dbf
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-27 21:57:30 +0800

    583. 两个字符串的删除操作 (Delete Operation for Two Strings)

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -6585,6 +6585,53 @@ public class LeetCode_4 {
 
     }
 
+    // 583. 两个字符串的删除操作 (Delete Operation for Two Strings) --最长公共子序列
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        // dp[i][j] ： word1[0,i] 和 word2[0,j] 的最长公共子序列
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; ++i) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j <= n; ++j) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return m - dp[m][n] + n - dp[m][n];
+    }
+
+    // 583. 两个字符串的删除操作 (Delete Operation for Two Strings) --dp
+    public int minDistance2(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        // dp[i][j] ： 使 word1[0,i] 和 word2[0,j] 相同，所需删除的最少字符
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; ++i) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j <= n; ++j) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+
+    }
+
     // 526. 优美的排列 (Beautiful Arrangement)
     // public int countArrangement(int n) {
 
