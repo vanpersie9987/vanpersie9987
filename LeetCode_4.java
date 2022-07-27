4710a2d00e588feee9061986735de8a65cb01244
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-07-27 22:47:54 +0800

    474. 一和零 (Ones and Zeroes) --0-1背包

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -6632,6 +6632,36 @@ public class LeetCode_4 {
 
     }
 
+    // 474. 一和零 (Ones and Zeroes) --0-1背包
+    public int findMaxForm(String[] strs, int m, int n) {
+        int len = strs.length;
+        // dp[i][j][k] ：在前i个字符串中，使用j个0和k个1可获得的最多字符串个数
+        int[][][] dp = new int[len + 1][m + 1][n + 1];
+        for (int i = 1; i <= len; ++i) {
+            int[] count = getCounts474(strs[i - 1]);
+            int zeroes = count[0];
+            int ones = count[1];
+            for (int j = 0; j <= m; ++j) {
+                for (int k = 0; k <= n; ++k) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (j >= zeroes && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i - 1][j - zeroes][k - ones] + 1, dp[i - 1][j][k]);
+                    }
+                }
+            }
+        }
+        return dp[len][m][n];
+
+    }
+
+    private int[] getCounts474(String str) {
+        int[] counts = new int[2];
+        for (char c : str.toCharArray()) {
+            ++counts[c - '0'];
+        }
+        return counts;
+    }
+
     // 526. 优美的排列 (Beautiful Arrangement)
     // public int countArrangement(int n) {
 
