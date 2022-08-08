38adad363e600ee3d2fd397eaf39b50e126b8d96
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-08-08 21:00:03 +0800

    300. 最长递增子序列 (Longest Increasing Subsequence) 不是最优解

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -19312,16 +19312,14 @@ public class LeetCodeText {
         // dp表示以dp[i]结尾的最长严格递增序列的长度
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
-        int res = 1;
         for (int i = 1; i < dp.length; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[j] < nums[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            res = Math.max(res, dp[i]);
         }
-        return res;
+        return dp[dp.length - 1];
 
     }
 
