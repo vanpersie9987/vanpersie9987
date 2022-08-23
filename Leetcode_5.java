75f7c5202bbeffbda4e4791a1e508d4f4fc8228a
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-08-24 07:57:41 +0800

    1845. 座位预约管理系统 (Seat Reservation Manager) --优先队列

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Leetcode_5 {
     public static void main(String[] args) {
@@ -60,5 +61,26 @@ public class Leetcode_5 {
             res.append(s);
         }
         return res.replace(0, 1, String.valueOf(Character.toUpperCase(res.charAt(0)))).toString();
+    }
+
+    // 1845. 座位预约管理系统 (Seat Reservation Manager) --优先队列
+    class SeatManager {
+        private PriorityQueue<Integer> queue;
+
+        public SeatManager(int n) {
+            queue = new PriorityQueue<>();
+            for (int i = 1; i <= n; ++i) {
+                queue.offer(i);
+            }
+        }
+
+        public int reserve() {
+            return queue.poll();
+
+        }
+
+        public void unreserve(int seatNumber) {
+            queue.offer(seatNumber);
+        }
     }
 }
