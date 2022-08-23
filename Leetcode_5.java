ab27503d9a54448df78f4e85bc7f2c24985f4d36
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-08-23 22:28:50 +0800

    创建——Leetcode_5.java

---
+++ Leetcode_5.java
@@ -0,0 +1,25 @@
+public class Leetcode_5 {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
