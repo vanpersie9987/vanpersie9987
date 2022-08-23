c587c28055ee773ea180d9c72e81cbb187281ce4
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-08-23 22:22:13 +0800

    916. 单词子集 (Word Subsets)

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -9968,4 +9968,34 @@ public class LeetCode_4 {
         return Math.max(Math.max(maxA - minA, maxB - minB), Math.max(maxC - minC, maxD - minD));
     }
 
+    // 916. 单词子集 (Word Subsets)
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] maxCounts = new int[26];
+        for (String word : words2) {
+            int[] counts = new int[26];
+            for (char c : word.toCharArray()) {
+                ++counts[c - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                maxCounts[i] = Math.max(maxCounts[i], counts[i]);
+            }
+        }
+
+        List<String> res = new ArrayList<>();
+        search: for (String word : words1) {
+            int[] counts = new int[26];
+            for (char c : word.toCharArray()) {
+                ++counts[c - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                if (counts[i] < maxCounts[i]) {
+                    continue search;
+                }
+            }
+            res.add(word);
+        }
+        return res;
+
+    }
+
 }
