3e6aef23549a0ea61456d7dd0ddfe45b5e2f6a7f
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-08-23 23:16:23 +0800

    1451. 重新排列句子中的单词 (Rearrange Words in a Sentence)

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Leetcode_5 {
     public static void main(String[] args) {
 
@@ -20,6 +26,39 @@ public class Leetcode_5 {
             this.left = left;
             this.right = right;
         }
+    }
+
+    // 1451. 重新排列句子中的单词 (Rearrange Words in a Sentence)
+    public String arrangeWords(String text) {
+        StringBuilder builder = new StringBuilder(text);
+        builder.replace(0, 1, String.valueOf(Character.toLowerCase(builder.charAt(0))));
+        List<String> list = new ArrayList<>();
+        int left = 0;
+        int right = 0;
+        while (right <= builder.length()) {
+            if (right == builder.length() || builder.charAt(right) == ' ') {
+                list.add(builder.substring(left, right));
+                left = right + 1;
+            }
+            ++right;
+        }
+        Collections.sort(list, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+
+        });
+
+        StringBuilder res = new StringBuilder();
+        for (String s : list) {
+            if (!res.isEmpty()) {
+                res.append(" ");
+            }
+            res.append(s);
+        }
+        return res.replace(0, 1, String.valueOf(Character.toUpperCase(res.charAt(0)))).toString();
     }
 
 }
