e15e2b1d0b8b49a94090608fdfd41e4f64c7e204
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-08-25 16:32:45 +0800

    优化

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -11,6 +11,21 @@ public class Leetcode_5 {
 
     }
 
+    public class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
     public class TreeNode {
         int val;
         TreeNode left;
