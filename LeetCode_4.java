b985c40433e3084b4f0a6bdd9c551ffa915e7949
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-08-26 21:14:56 +0800

    1760. 袋子里最少数目的球 (Minimum Limit of Balls in a Bag)

--- LeetCode_4.java
+++ LeetCode_4.java
@@ -9997,4 +9997,30 @@ public class LeetCode_4 {
 
     }
 
+    // 1760. 袋子里最少数目的球 (Minimum Limit of Balls in a Bag)
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1;
+        int right = (int) 1e9;
+        int res = -1;
+        while (left <= right) {
+            int mid = left + ((right - left) >>> 1);
+            if (judge1760(nums, mid) <= maxOperations) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+
+    }
+
+    private int judge1760(int[] nums, int max) {
+        int operations = 0;
+        for (int num : nums) {
+            operations += (num - 1) / max;
+        }
+        return operations;
+    }
+
 }
