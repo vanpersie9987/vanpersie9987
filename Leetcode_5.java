fe8da494ae8f737da246a74064e4ac7c0d663a32
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-09-15 14:09:51 +0800

    361. 轰炸敌人 (Bomb Enemy) --plus dp

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -1955,4 +1955,63 @@ public class Leetcode_5 {
         }
         return presses == 1 ? 4 : (presses == 2 ? 7 : 8);
     }
+
+    // 361. 轰炸敌人 (Bomb Enemy) --plus dp
+    public int maxKilledEnemies(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        int pre = 0;
+        for (int i = 0; i < m; ++i) {
+            pre = 0;
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 'W') {
+                    pre = 0;
+                } else if (grid[i][j] == 'E') {
+                    ++pre;
+                }
+                dp[i][j] += pre;
+            }
+            pre = 0;
+            for (int j = n - 1; j >= 0; --j) {
+                if (grid[i][j] == 'W') {
+                    pre = 0;
+                } else if (grid[i][j] == 'E') {
+                    ++pre;
+                }
+                dp[i][j] += pre;
+            }
+        }
+
+        for (int j = 0; j < n; ++j) {
+            pre = 0;
+            for (int i = 0; i < m; ++i) {
+                if (grid[i][j] == 'W') {
+                    pre = 0;
+                } else if (grid[i][j] == 'E') {
+                    ++pre;
+                }
+                dp[i][j] += pre;
+            }
+            pre = 0;
+            for (int i = m - 1; i >= 0; --i) {
+                if (grid[i][j] == 'W') {
+                    pre = 0;
+                } else if (grid[i][j] == 'E') {
+                    ++pre;
+                }
+                dp[i][j] += pre;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '0') {
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+        return res;
+
+    }
 }
