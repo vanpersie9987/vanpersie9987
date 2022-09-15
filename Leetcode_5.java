0f60edf41bef9aa0b545f34f0c759629dc3fa2f6
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-09-15 13:31:10 +0800

    672. 灯泡开关 Ⅱ (Bulb Switcher II)

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -1941,4 +1941,18 @@ public class Leetcode_5 {
         }
         return false;
     }
+
+    // 672. 灯泡开关 Ⅱ (Bulb Switcher II)
+    public int flipLights(int n, int presses) {
+        if (presses == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 2;
+        }
+        if (n == 2) {
+            return presses == 1 ? 3 : 4;
+        }
+        return presses == 1 ? 4 : (presses == 2 ? 7 : 8);
+    }
 }
