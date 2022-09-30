375d228f7a4507d1c37f4f286e7819b70471f775
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-09-30 08:38:04 +0800

    LCP 61. 气温变化趋势

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -2985,6 +2985,25 @@ public class Leetcode_5 {
         builder.deleteCharAt(builder.length() - 1);
     }
 
+    // LCP 61. 气温变化趋势
+    public int temperatureTrend(int[] temperatureA, int[] temperatureB) {
+        int n = temperatureA.length;
+        int res = 0;
+        int cur = 0;
+        for (int i = 1; i < n; ++i) {
+            if (temperatureA[i] > temperatureA[i - 1] && temperatureB[i] > temperatureB[i - 1]
+                    || temperatureA[i] == temperatureA[i - 1] && temperatureB[i] == temperatureB[i - 1]
+                    || temperatureA[i] < temperatureA[i - 1] && temperatureB[i] < temperatureB[i - 1]) {
+                ++cur;
+            } else {
+                cur = 0;
+            }
+            res = Math.max(res, cur);
+        }
+        return res;
+
+    }
+
     // 2029. 石子游戏 IX (Stone Game IX)
     // public boolean stoneGameIX(int[] stones) {
 
