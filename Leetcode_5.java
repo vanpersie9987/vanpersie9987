72e775e52d5b6987a0ad1ea11d50994c71e125cc
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-10-04 13:06:12 +0800

    1754. 构造字典序最大的合并字符串 (Largest Merge Of Two Strings) --双指针

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -3836,6 +3836,30 @@ public class Leetcode_5 {
      
     }
 
+    // 1754. 构造字典序最大的合并字符串 (Largest Merge Of Two Strings) --双指针
+    public String largestMerge(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int i = 0;
+        int j = 0;
+        StringBuilder res = new StringBuilder();
+        while (i < m && j < n) {
+            if (word1.substring(i).compareTo(word2.substring(j)) > 0) {
+                res.append(word1.charAt(i++));
+            } else {
+                res.append(word2.charAt(j++));
+            }
+        }
+        while (i < m) {
+            res.append(word1.charAt(i++));
+        }
+        while (j < n) {
+            res.append(word2.charAt(j++));
+        }
+        return res.toString();
+
+    }
+
     // 2029. 石子游戏 IX (Stone Game IX)
     // public boolean stoneGameIX(int[] stones) {
 
