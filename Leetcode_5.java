e812046edc1767e9fd246af4b7fba49d60772468
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-10-06 20:07:52 +0800

    376. 摆动序列 (Wiggle Subsequence) --空间：O(1) 动态规划

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -4091,7 +4091,7 @@ public class Leetcode_5 {
         return i;
     }
 
-    // 376. 摆动序列 (Wiggle Subsequence) --O(n)
+    // 376. 摆动序列 (Wiggle Subsequence) --空间：O(n) 动态规划
     public int wiggleMaxLength(int[] nums) {
         int n = nums.length;
         if (n <= 1) {
@@ -4113,6 +4113,25 @@ public class Leetcode_5 {
             }
         }
         return Math.max(up[n - 1], down[n - 1]);
+
+    }
+
+    // 376. 摆动序列 (Wiggle Subsequence) --空间：O(1) 动态规划
+    public int wiggleMaxLength2(int[] nums) {
+        int n = nums.length;
+        if (n <= 1) {
+            return n;
+        }
+        int up = 1;
+        int down = 1;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > nums[i - 1]) {
+                down = Math.max(down, up + 1);
+            } else if (nums[i] < nums[i - 1]) {
+                up = Math.max(up, down + 1);
+            }
+        }
+        return Math.max(up, down);
 
     }
 
