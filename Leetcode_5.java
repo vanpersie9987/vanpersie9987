972136a9ab3de54e4a581e870b4de0535eb88cb1
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-10-06 19:58:46 +0800

    376. 摆动序列 (Wiggle Subsequence) --O(n)

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -4091,6 +4091,31 @@ public class Leetcode_5 {
         return i;
     }
 
+    // 376. 摆动序列 (Wiggle Subsequence) --O(n)
+    public int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n <= 1) {
+            return n;
+        }
+        int[] up = new int[n];
+        int[] down = new int[n];
+        up[0] = down[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > nums[i - 1]) {
+                down[i] = Math.max(down[i - 1], up[i - 1] + 1);
+                up[i] = up[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return Math.max(up[n - 1], down[n - 1]);
+
+    }
+
     // 2029. 石子游戏 IX (Stone Game IX)
     // public boolean stoneGameIX(int[] stones) {
 
