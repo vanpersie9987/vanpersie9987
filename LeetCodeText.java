51bdd2b3fad422a167fb4437fa0914f31cf818a9
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-10-22 21:39:33 +0800

    123. 买卖股票的最佳时机 III

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -1032,15 +1032,14 @@ public class LeetCodeText {
 
     // 123. 买卖股票的最佳时机 III
     public int maxProfit3(int[] prices) {
-        int buy1 = -prices[0];
-        int sell1 = 0;
-        int buy2 = -prices[0];
-        int sell2 = 0;
-        for (int i = 0; i < prices.length; ++i) {
-            buy1 = Math.max(buy1, -prices[i]);
-            sell1 = Math.max(sell1, buy1 + prices[i]);
-            buy2 = Math.max(buy2, sell1 - prices[i]);
-            sell2 = Math.max(sell2, buy2 + prices[i]);
+        int n = prices.length;
+        int buy1 = prices[0], sell1 = 0;
+        int buy2 = prices[0], sell2 = 0;
+        for (int i = 1; i < n; ++i) {
+            buy1 = Math.min(buy1, prices[i]);
+            sell1 = Math.max(sell1, prices[i] - buy1);
+            buy2 = Math.min(buy2, prices[i] - sell1);
+            sell2 = Math.max(sell2, prices[i] - buy2);
         }
         return sell2;
 
