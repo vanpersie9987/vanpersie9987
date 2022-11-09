7364454fea1e55b0822856d535ca75579d9a76ca
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-11-09 21:20:47 +0800

    1913. 两个数对之间的最大乘积差 (Maximum Product Difference Between Two Pairs)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -18624,22 +18624,22 @@ public class LeetCodeText {
 
     // 1913. 两个数对之间的最大乘积差 (Maximum Product Difference Between Two Pairs)
     public int maxProductDifference(int[] nums) {
-        int max1 = Integer.MIN_VALUE;
-        int max2 = Integer.MIN_VALUE;
         int min1 = Integer.MAX_VALUE;
         int min2 = Integer.MAX_VALUE;
+        int max1 = 0;
+        int max2 = 0;
         for (int num : nums) {
             if (num >= max1) {
                 max2 = max1;
                 max1 = num;
-            } else if (num >= max2) {
+            } else if (num > max2) {
                 max2 = num;
             }
 
             if (num <= min1) {
                 min2 = min1;
                 min1 = num;
-            } else if (num <= min2) {
+            } else if (num < min2) {
                 min2 = num;
             }
         }
