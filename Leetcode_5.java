15d5b13004d2188f6588e85d74c9ce1305f359cf
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-11-12 15:33:31 +0800

    790. 多米诺和托米诺平铺 (Domino and Tromino Tiling) --dp

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -7578,6 +7578,22 @@ public class Leetcode_5 {
 
     }
 
+    // 790. 多米诺和托米诺平铺 (Domino and Tromino Tiling) --dp
+    public int numTilings(int n) {
+        final int mod = (int) (1e9 + 7);
+        int[][] dp = new int[n + 1][4];
+        dp[0][3] = 1;
+        for (int i = 1; i <= n; ++i) {
+            dp[i][0] = dp[i - 1][3];
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+            dp[i][3] = ((dp[i - 1][0] % mod + dp[i - 1][1] % mod) % mod
+                    + (dp[i - 1][2] % mod + dp[i - 1][3] % mod) % mod) % mod;
+        }
+        return dp[n][3];
+
+    }
+
     // 6232. 最小移动总距离
     // public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
 
