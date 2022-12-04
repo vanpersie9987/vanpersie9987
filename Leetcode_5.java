72f331266b40f2619bf6b70882818ccd501496f8
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-12-04 15:43:43 +0800

    新建-leetcode_6

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -10075,39 +10075,4 @@ public class Leetcode_5 {
 
     }
 
-    // 1648. 销售价值减少的颜色球 (Sell Diminishing-Valued Colored Balls)
-    // public int maxProfit(int[] inventory, int orders) {
-
-    // }
-
-    // 1562. 查找大小为 M 的最新分组 (Find Latest Group of Size M)
-    // public int findLatestStep(int[] arr, int m) {
-
-    // }
-
-    // 813. 最大平均值和的分组 (Largest Sum of Averages)
-    // public double largestSumOfAverages(int[] nums, int k) {
-
-    // }
-
-    // 2484. 统计回文子序列数目 (Count Palindromic Subsequences)
-    // public int countPalindromes(String s) {
-
-    // }
-
-    // 2488. 统计中位数为 K 的子数组 (Count Subarrays With Median K)
-    // public int countSubarrays(int[] nums, int k) {
-
-    // }
-
-    // 2467. 树上最大得分和路径 (Most Profitable Path in a Tree)
-    // public int mostProfitablePath(int[][] edges, int bob, int[] amount) {
-
-    // }
-
-    // 2466. 统计构造好字符串的方案数 (Count Ways To Build Good Strings)
-    // public int countGoodStrings(int low, int high, int zero, int one) {
-
-    // }
-
 }
---
+++ Leetcode_6.java
@@ -0,0 +1,75 @@
+public class Leetcode_6 {
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 1648. 销售价值减少的颜色球 (Sell Diminishing-Valued Colored Balls)
+    // public int maxProfit(int[] inventory, int orders) {
+
+    // }
+
+    // 1562. 查找大小为 M 的最新分组 (Find Latest Group of Size M)
+    // public int findLatestStep(int[] arr, int m) {
+
+    // }
+
+    // 813. 最大平均值和的分组 (Largest Sum of Averages)
+    // public double largestSumOfAverages(int[] nums, int k) {
+
+    // }
+
+    // 2484. 统计回文子序列数目 (Count Palindromic Subsequences)
+    // public int countPalindromes(String s) {
+
+    // }
+
+    // 2488. 统计中位数为 K 的子数组 (Count Subarrays With Median K)
+    // public int countSubarrays(int[] nums, int k) {
+
+    // }
+
+    // 2467. 树上最大得分和路径 (Most Profitable Path in a Tree)
+    // public int mostProfitablePath(int[][] edges, int bob, int[] amount) {
+
+    // }
+
+    // 2466. 统计构造好字符串的方案数 (Count Ways To Build Good Strings)
+    // public int countGoodStrings(int low, int high, int zero, int one) {
+
+    // }
+
+}
