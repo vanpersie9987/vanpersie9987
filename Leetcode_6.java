ccdb9f7f816cf395e987461d2cbaf4fea6edefef
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-12-05 10:58:00 +0800

    261. 以图判树 (Graph Valid Tree) --plus 是否有多个连通分量 (若无重边或自环，则只需判断「节点个数 - 1 ==     // 边的条数」)、是否有环

--- Leetcode_6.java
+++ Leetcode_6.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 public class Leetcode_6 {
     public static void main(String[] args) {
 
@@ -62,6 +69,44 @@ public class Leetcode_6 {
         }
         return res;
 
+    }
+
+    // 261. 以图判树 (Graph Valid Tree) --plus 是否有多个连通分量 (若无重边或自环，则只需判断「节点个数 - 1 ==
+    // 边的条数」)、是否有环
+    public boolean validTree(int n, int[][] edges) {
+        return n - 1 == edges.length && checkCycle261(n, edges);
+
+    }
+
+    private boolean checkCycle261(int n, int[][] edges) {
+        if (n == 1) {
+            return true;
+        }
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int[] indegrees = new int[n];
+        for (int[] edge : edges) {
+            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+            ++indegrees[edge[0]];
+            ++indegrees[edge[1]];
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            if (--indegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            --n;
+            for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
+                --indegrees[neighbor];
+                if (indegrees[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return n == 0;
     }
 
     // 1648. 销售价值减少的颜色球 (Sell Diminishing-Valued Colored Balls)
