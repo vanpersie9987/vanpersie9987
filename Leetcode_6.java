865840b8f67d0d281a9a5053ffebfd141d886723
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-12-09 20:10:00 +0800

    LCP 56. 信物传送

--- Leetcode_6.java
+++ Leetcode_6.java
@@ -11,7 +11,6 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Random;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Leetcode_6 {
