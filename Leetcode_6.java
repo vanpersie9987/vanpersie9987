dc72f679901320f3896c45b5e1a59fc24f3fe1c2
liumingyuan <mingyuan.liu@lingyue-digital.com>
2022-12-18 20:04:33 +0800

    923. 三数之和的多种可能 (3Sum With Multiplicity)

--- Leetcode_6.java
+++ Leetcode_6.java
@@ -2098,15 +2098,49 @@ public class Leetcode_6 {
     }
 
     // 923. 三数之和的多种可能 (3Sum With Multiplicity)
-    // public int threeSumMulti(int[] arr, int target) {
-    // int[] counts = new int[101];
-    // for (int a : arr) {
-    // ++counts[a];
-    // }
-    // long res = 0l;
-    // final int mod = (int) (1e9 + 7);
+    public int threeSumMulti(int[] arr, int target) {
+        int[] counts = new int[101];
+        for (int a : arr) {
+            ++counts[a];
+        }
+        long res = 0l;
+        final int mod = (int) (1e9 + 7);
 
-    // }
+        // x != y != z
+        for (int x = 0; x <= 100; ++x) {
+            for (int y = x + 1; y <= 100; ++y) {
+                int z = target - x - y;
+                if (y < z && z <= 100) {
+                    res = (res + (long) counts[x] * counts[y] * counts[z]) % mod;
+                }
+            }
+        }
+        // x == y != z
+        for (int x = 0; x <= 100; ++x) {
+            int z = target - 2 * x;
+            if (x < z && z <= 100) {
+                res = (res + (long) counts[x] * (counts[x] - 1) / 2 * counts[z]) % mod;
+            }
+        }
+        // x != y == z
+        for (int x = 0; x <= 100; ++x) {
+            if (target % 2 == x % 2) {
+                int y = (target - x) / 2;
+                if (x < y && y <= 100) {
+                    res = (res + (long) counts[x] * counts[y] * (counts[y] - 1) / 2) % mod;
+                }
+            }
+        }
+
+        // x == y == z
+        for (int x = 0; x <= 100; ++x) {
+            if (x * 3 == target) {
+                res = (res + (long) counts[x] * (counts[x] - 1) * (counts[x] - 2) / 6) % mod;
+            }
+        }
+        return (int) (res % mod);
+
+    }
 
     // 2250. 统计包含每个点的矩形数目 (Count Number of Rectangles Containing Each Point)
     // public int[] countRectangles(int[][] rectangles, int[][] points) {
