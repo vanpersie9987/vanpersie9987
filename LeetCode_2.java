b9b081e4e863ffb80d0854e6de5cb01af8bdd294
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-02-04 11:15:17 +0800

    1798. 你能构造出连续值的最大数目 (Maximum Number of Consecutive Values You Can Make)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -8391,14 +8391,14 @@ public class LeetCode_2 {
    // 1798. 你能构造出连续值的最大数目 (Maximum Number of Consecutive Values You Can Make)
    public int getMaximumConsecutive(int[] coins) {
       Arrays.sort(coins);
-      int res = 0;
+      int res = 1;
       for (int coin : coins) {
-         if (coin > res + 1) {
+         if (coin > res) {
             break;
          }
          res += coin;
       }
-      return res + 1;
+      return res;
 
    }
 
