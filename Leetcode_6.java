d617655c098f8b3c1aac0487c96cbbef6a056093
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-02-18 19:05:58 +0800

    Number of Paths with Max Score

--- Leetcode_6.java
+++ Leetcode_6.java
@@ -7432,4 +7432,82 @@ public class Leetcode_6 {
     //     return false;
 
     // }
+
+    // 1301. 最大得分的路径数目 (Number of Paths with Max Score)
+    // private int n;
+    // private List<String> board;
+    // private int[][] memo;
+    // private int maxSum;
+
+    // public int[] pathsWithMaxScore(List<String> board) {
+    //     this.n = board.size();
+    //     this.board = board;
+    //     this.memo = new int[n][n];
+    //     for (int i = 0; i < n; ++i) {
+    //         Arrays.fill(memo[i], -1);
+    //     }
+    //     dfs(n - 1, n - 1, 0);
+    //     if (maxSum == 0) {
+    //         return new int[] { 0, 0 };
+    //     }
+    //     for (int i = 0; i < n; ++i) {
+    //         Arrays.fill(memo[i], -1);
+    //     }
+    //     return new int[] { maxSum, dfs2(n - 1, n - 1, maxSum) };
+
+    // }
+
+    // private int dfs2(int i, int j, int sum) {
+    //     if (i < 0 || j < 0 || board.get(i).charAt(j) == 'X') {
+    //         return 0;
+    //     }
+    //     if (i == 0 && j == 0) {
+    //         if (sum == 0) {
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
+    //     if (memo[i][j] != -1) {
+    //         return memo[i][j];
+    //     }
+
+    //     int cur = 0;
+    //     final int MOD = (int) (1e9 + 7);
+    //     if (i == n - 1 && j == n - 1) {
+    //         cur = (cur + dfs2(i - 1, j - 1, sum)) % MOD;
+    //         cur = (cur + dfs2(i - 1, j, sum)) % MOD;
+    //         cur = (cur + dfs2(i, j - 1, sum)) % MOD;
+    //         return cur;
+    //     }
+    //     cur = sum - (board.get(i).charAt(j) - '0');
+    //     int count = 0;
+    //     count = (count + dfs2(i - 1, j, cur)) % MOD;
+    //     count = (count + dfs2(i, j - 1, cur)) % MOD;
+    //     count = (count + dfs2(i - 1, j - 1, cur)) % MOD;
+    //     return memo[i][j] = count;
+    // }
+
+    // private int dfs(int i, int j, int sum) {
+    //     if (i == n - 1 && j == n - 1) {
+    //         return Math.max(dfs(i - 1, j - 1, sum), Math.max(dfs(i - 1, j, sum), dfs(i, j - 1, sum)));
+    //     }
+    //     if (i < 0 || j < 0 || board.get(i).charAt(j) == 'X') {
+    //         return 0;
+    //     }
+
+    //     if (i == 0 && j == 0) {
+    //         maxSum = Math.max(maxSum, sum);
+    //         return maxSum;
+    //     }
+
+    //     int cur = board.get(i).charAt(j) - '0' + sum;
+    //     if (memo[i][j] >= cur) {
+    //         return 0;
+    //     }
+    //     int max = 0;
+    //     max = Math.max(max, dfs(i - 1, j, cur));
+    //     max = Math.max(max, dfs(i, j - 1, cur));
+    //     max = Math.max(max, dfs(i - 1, j - 1, cur));
+    //     return memo[i][j] = max;
+    // }
 }
