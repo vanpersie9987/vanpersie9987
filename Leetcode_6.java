fadade5306e94bf3f636c6f91591860c30f60251
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-02-23 14:02:34 +0800

    1444. 切披萨的方案数 (Number of Ways of Cutting a Pizza)

--- Leetcode_6.java
+++ Leetcode_6.java
@@ -7618,77 +7618,69 @@ public class Leetcode_6 {
         return memo1140[i][m] = piles1140[i] - min;
     }
 
-
-    // 1712. 将数组分成三个子数组的方案数 (Ways to Split Array Into Three Subarrays)
-    // public int waysToSplit(int[] nums) {
-
-    // }
-
-    // 2402. 会议室 III (Meeting Rooms III)
-    // public int mostBooked(int n, int[][] meetings) {
-
-    // }
-
     // 1444. 切披萨的方案数 (Number of Ways of Cutting a Pizza)
-    // private int[][][] memo;
-    // private int[][] pre;
-    // private int m;
-    // private int n;
-    // private final int MOD = (int) (1e9 + 7);
+    private int[][][] memo1444;
+    private int[][] pre1444;
+    private int m1444;
+    private int n1444;
 
-    // public int ways(String[] pizza, int k) {
-    // this.m = pizza.length;
-    // this.n = pizza[0].length();
-    // memo = new int[m][n][k];
-    // for (int i = 0; i < m; ++i) {
-    // for (int j = 0; j < n; ++j) {
-    // Arrays.fill(memo[i][j], -1);
-    // }
-    // }
-    // this.pre = new int[m + 1][n + 1];
-    // for (int i = 1; i < m + 1; ++i) {
-    // for (int j = 1; j < n + 1; ++j) {
-    // pre[i][j] = pre[i - 1][j] + pre[i][j - 1] + (pizza[i - 1].charAt(j - 1) ==
-    // 'A' ? 1 : 0);
-    // }
-    // }
+    public int ways(String[] pizza, int k) {
+        this.m1444 = pizza.length;
+        this.n1444 = pizza[0].length();
+        memo1444 = new int[m1444][n1444][k];
+        for (int i = 0; i < m1444; ++i) {
+            for (int j = 0; j < n1444; ++j) {
+                Arrays.fill(memo1444[i][j], -1);
+            }
+        }
+        this.pre1444 = new int[m1444 + 1][n1444 + 1];
+        for (int i = 1; i < m1444 + 1; ++i) {
+            for (int j = 1; j < n1444 + 1; ++j) {
+                pre1444[i][j] = pre1444[i - 1][j] + pre1444[i][j - 1] - pre1444[i - 1][j - 1]
+                        + (pizza[i - 1].charAt(j - 1) == 'A' ? 1 : 0);
+            }
+        }
 
-    // return dfs(0, 0, k - 1);
+        return dfs1444(0, 0, k - 1);
 
-    // }
+    }
 
-    // private int dfs(int x, int y, int left) {
-    // int count = getApplesCounts(x, y, m - 1, n - 1);
-    // if (left == 0) {
-    // return count > 0 ? 1 : 0;
-    // }
-    // if (count < left + 1) {
-    // return 0;
-    // }
-    // if (memo[x][y][left] != -1) {
-    // return memo[x][y][left];
-    // }
-    // memo[x][y][left] = 0;
-    // for (int i = x; i < m - 1; ++i) {
-    // int c = getApplesCounts(x, y, i, n - 1);
-    // if (c == 0) {
-    // continue;
-    // }
-    // memo[x][y][left] = (memo[x][y][left] + dfs(i + 1, y, left - 1)) % MOD;
-    // }
-    // for (int j = y; j < n - 1; ++j) {
-    // int c = getApplesCounts(x, y, m - 1, j);
-    // if (c == 0) {
-    // continue;
-    // }
-    // memo[x][y][left] = (memo[x][y][left] + dfs(x, j + 1, left - 1)) % MOD;
-    // }
-    // return memo[x][y][left];
-    // }
+    private int dfs1444(int x, int y, int left) {
+        int count = getApplesCounts(x, y, m1444 - 1, n1444 - 1);
+        if (left == 0) {
+            return count > 0 ? 1 : 0;
+        }
+        if (count < left + 1) {
+            return 0;
+        }
+        if ((m1444 - x - 1) + (n1444 - y - 1) < left) {
+            return 0;
+        }
+        if (memo1444[x][y][left] != -1) {
+            return memo1444[x][y][left];
+        }
+        final int MOD = (int) (1e9 + 7);
+        int res = 0;
+        for (int i = x; i < m1444 - 1; ++i) {
+            int c = getApplesCounts(x, y, i, n1444 - 1);
+            if (c == 0) {
+                continue;
+            }
+            res = (res + dfs1444(i + 1, y, left - 1)) % MOD;
+        }
+        for (int j = y; j < n1444 - 1; ++j) {
+            int c = getApplesCounts(x, y, m1444 - 1, j);
+            if (c == 0) {
+                continue;
+            }
+            res = (res + dfs1444(x, j + 1, left - 1)) % MOD;
+        }
+        return memo1444[x][y][left] = res;
+    }
 
-    // private int getApplesCounts(int x1, int y1, int x2, int y2) {
-    // return pre[x2 + 1][y2 + 1] - pre[x2 + 1][y1] - pre[x1][y2 + 1] + pre[x1][y1];
-    // }
+    private int getApplesCounts(int x1, int y1, int x2, int y2) {
+        return pre1444[x2 + 1][y2 + 1] - pre1444[x2 + 1][y1] - pre1444[x1][y2 + 1] + pre1444[x1][y1];
+    }
 
     // LCP 52. 二叉搜索树染色
     // public int getNumber(TreeNode root, int[][] ops) {
@@ -7748,6 +7740,16 @@ public class Leetcode_6 {
     // }
     // }
     // return false;
+
+    // }
+
+    // 1712. 将数组分成三个子数组的方案数 (Ways to Split Array Into Three Subarrays)
+    // public int waysToSplit(int[] nums) {
+
+    // }
+
+    // 2402. 会议室 III (Meeting Rooms III)
+    // public int mostBooked(int n, int[][] meetings) {
 
     // }
 }
