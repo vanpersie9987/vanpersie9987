f8a953edc3ef6e1df12d64a2a2e7590e29819438
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-03-23 22:09:40 +0800

    2251. 花期内花的数目 (Number of Flowers in Full Bloom)

--- Leetcode_7.java
+++ Leetcode_7.java
@@ -812,6 +812,34 @@ public class Leetcode_7 {
         return memo1335[days][jobIndex] = res;
     }
 
+    // 2251. 花期内花的数目 (Number of Flowers in Full Bloom)
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        int n = people.length;
+        TreeMap<Integer, Integer> diff = new TreeMap<>();
+        diff.put(0, 0);
+        for (int[] flower : flowers) {
+            int start = flower[0];
+            int end = flower[1];
+            diff.merge(start, 1, Integer::sum);
+            diff.merge(end + 1, -1, Integer::sum);
+        }
+        int pre = 0;
+        for (Map.Entry<Integer, Integer> entry : diff.entrySet()) {
+            int key = entry.getKey();
+            int val = entry.getValue();
+            pre += val;
+            diff.put(key, pre);
+        }
+        int[] res = new int[n];
+        for (int i = 0; i < n; ++i) {
+            Integer val = diff.floorKey(people[i]);
+            res[i] = diff.getOrDefault(val, 0);
+        }
+        return res;
+
+    }
+
+
     // 1363. 形成三的最大倍数 (Largest Multiple of Three)
     // public String largestMultipleOfThree(int[] digits) {
 
@@ -951,5 +979,4 @@ public class Leetcode_7 {
     // }
     // return memo[i][j] = Math.max(dfs(b, i + 1, j), dfs(b, i, j - 1));
     // }
-
 }
