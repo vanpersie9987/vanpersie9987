a0c97b2d9820e8504b62ed7e7b70400dc3a41fc7
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-04-11 14:37:31 +0800

    面试题 17.24. 最大子矩阵 (Max Submatrix LCCI)

--- Leetcode_7.java
+++ Leetcode_7.java
@@ -2514,7 +2514,39 @@ public class Leetcode_7 {
         return f[2];
 
     }
-    
+
+    // 面试题 17.24. 最大子矩阵 (Max Submatrix LCCI)
+    public int[] getMaxMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] pre = new int[m][n];
+        int[] res = null;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                pre[i][j] += matrix[i][j] + (i - 1 >= 0 ? pre[i - 1][j] : 0);
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int up = 0; up < m; ++up) {
+            for (int down = up; down < m; ++down) {
+                int[] minPre = new int[] { -1, 0 };
+                int curPre = 0;
+                for (int j = 0; j < n; ++j) {
+                    curPre += pre[down][j] - (up - 1 >= 0 ? pre[up - 1][j] : 0);
+                    if (curPre - minPre[1] > max) {
+                        max = curPre - minPre[1];
+                        res = new int[] { up, minPre[0] + 1, down, j };
+                    }
+                    if (curPre < minPre[1]) {
+                        minPre = new int[] { j, curPre };
+                    }
+                }
+            }
+        }
+        return res;
+
+    }
+
 
     // 372. 超级次方 (Super Pow)
     // public int superPow(int a, int[] b) {
