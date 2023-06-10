11df0579ed6e6331293675418ded7ead7b2cc6bf
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-06-10 21:33:24 +0800

    6292. 子矩阵元素加 1 --二维差分

--- Leetcode_6.java
+++ Leetcode_6.java
@@ -4828,34 +4828,24 @@ public class Leetcode_6 {
         return sum;
     }
 
-    // 6292. 子矩阵元素加 1 --二维差分数组
+    // 6292. 子矩阵元素加 1 --二维差分
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] diff = new int[n + 1][n + 1];
-        for (int[] query : queries) {
-            int r1 = query[0];
-            int c1 = query[1];
-            int r2 = query[2];
-            int c2 = query[3];
-            ++diff[r1][c1];
-            --diff[r1][c2 + 1];
-            --diff[r2 + 1][c1];
-            ++diff[r2 + 1][c2 + 1];
-        }
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                diff[i][j] += diff[i][j - 1];
-            }
-        }
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                diff[i][j] += diff[i - 1][j];
-            }
+        int[][] diff = new int[n + 2][n + 2];
+        for (int[] q : queries) {
+            int r1 = q[0];
+            int c1 = q[1];
+            int r2 = q[2];
+            int c2 = q[3];
+            diff[r1 + 1][c1 + 1] += 1;
+            diff[r1 + 1][c2 + 2] -= 1;
+            diff[r2 + 2][c1 + 1] -= 1;
+            diff[r2 + 2][c2 + 2] += 1;
         }
         int[][] res = new int[n][n];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                res[i][j] = diff[i][j];
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
+                res[i - 1][j - 1] = diff[i][j];
             }
         }
         return res;
