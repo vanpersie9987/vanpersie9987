0c65e9ca77f8d4f4e3ae163451ad6e19ec9c56dd
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-06-21 20:25:44 +0800

    create Leetcode_8.java

---
+++ Leetcode_8.java
@@ -0,0 +1,39 @@
+public class Leetcode_8 {
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
