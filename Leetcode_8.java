2038651768013b7e1f7077cb6167948bf96ff72b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-06-22 11:09:02 +0800

    面试题 17.25. 单词矩阵 (Word Rectangle LCCI)

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class Leetcode_8 {
     public static void main(String[] args) {
 
@@ -35,5 +44,111 @@ public class Leetcode_8 {
             this.left = left;
             this.right = right;
         }
+    }
+
+    // 面试题 17.25. 单词矩阵 (Word Rectangle LCCI)
+    private List<String> res17_25;
+    private Trie17_25 root17_25;
+    private TreeMap<Integer, Set<String>> treeMap17_25;
+    private int maxArea17_25;
+
+    public String[] maxRectangle(String[] words) {
+        this.res17_25 = new ArrayList<>();
+        this.root17_25 = new Trie17_25();
+        this.treeMap17_25 = new TreeMap<>(new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o2, o1);
+            }
+
+        });
+        for (String word : words) {
+            root17_25.insert(word);
+            treeMap17_25.computeIfAbsent(word.length(), k -> new HashSet<>()).add(word);
+        }
+        for (Map.Entry<Integer, Set<String>> entry : treeMap17_25.entrySet()) {
+            dfs17_25(entry.getValue(), new ArrayList<>(), entry.getKey());
+        }
+        return res17_25.toArray(new String[0]);
+
+    }
+
+    private void dfs17_25(Set<String> set, List<String> path, int len) {
+        if (set.size() * len <= maxArea17_25) {
+            return;
+        }
+        if (path.size() > len) {
+            return;
+        }
+        for (String word : set) {
+            path.add(word);
+            boolean[] check = checkLegal17_25(path);
+            if (check[0]) {
+                if (check[1] && path.size() * len > maxArea17_25) {
+                    maxArea17_25 = path.size() * len;
+                    res17_25 = new ArrayList<>(path);
+                }
+                dfs17_25(set, path, len);
+            }
+            path.remove(path.size() - 1);
+        }
+
+    }
+
+    private boolean[] checkLegal17_25(List<String> path) {
+        int m = path.size();
+        int n = path.get(0).length();
+        boolean[] res = new boolean[2];
+        Arrays.fill(res, true);
+        for (int j = 0; j < n; ++j) {
+            Trie17_25 node = root17_25;
+            for (int i = 0; i < m; ++i) {
+                int index = path.get(i).charAt(j) - 'a';
+                if (node.children[index] == null) {
+                    Arrays.fill(res, false);
+                    return res;
+                }
+                node = node.children[index];
+            }
+            if (!node.isWord) {
+                res[1] = false;
+            }
+        }
+        return res;
+    }
+
+    public class Trie17_25 {
+        private Trie17_25[] children;
+        private boolean isWord;
+
+        public Trie17_25() {
+            children = new Trie17_25[26];
+        }
+
+        public void insert(String s) {
+            Trie17_25 node = this;
+            for (char c : s.toCharArray()) {
+                int index = c - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie17_25();
+                }
+                node = node.children[index];
+            }
+            node.isWord = true;
+        }
+
+        public boolean isLegal(String s) {
+            Trie17_25 node = this;
+            for (char c : s.toCharArray()) {
+                int index = c - 'a';
+                if (node.children[index] == null) {
+                    return false;
+                }
+                node = node.children[index];
+            }
+            return node.isWord;
+        }
+
     }
 }
