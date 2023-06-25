10b702ca34ea200b670fca528c13f86af155f537
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-06-26 00:20:48 +0800

    2485. 找出中枢整数 (Find the Pivot Integer)

--- Leetcode_5.java
+++ Leetcode_5.java
@@ -9269,11 +9269,8 @@ public class Leetcode_5 {
 
     // 2485. 找出中枢整数 (Find the Pivot Integer)
     public int pivotInteger(int n) {
-        int sum = (1 + n) * n / 2;
-        int preSum = 0;
         for (int i = 1; i <= n; ++i) {
-            preSum += i;
-            if (preSum == sum - preSum + i) {
+            if ((1 + i) * i == (i + n) * (n - i + 1)) {
                 return i;
             }
         }
