f0e5c944244e75b11e0c4e9269e93b5db87c9e18
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-07-03 00:09:44 +0800

    2193. 得到回文串的最少操作次数 (Minimum Number of Moves to Make Palindrome)

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -1373,38 +1373,4 @@ public class Leetcode_8 {
     // return 0;
     // }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        l1 = reverse(l1);
-        l2 = reverse(l2);
-        ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            if (l1 != null) {
-                carry += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                carry += l2.val;
-                l2 = l2.next;
-            }
-            ListNode node = new ListNode(carry % 10);
-            carry /= 10;
-            cur.next = node;
-            cur = cur.next;
-        }
-        return reverse(dummy.next);
-
-    }
-
-    private ListNode reverse(ListNode node) {
-        ListNode pre = null;
-        while (node != null) {
-            ListNode next = node.next;
-            node.next = pre;
-            pre = node;
-            node = next;
-        }
-        return pre;
-    }
 }
