c1e292a1a47fd28f7b569b87627501aca7d5cd04
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-07-07 20:55:51 +0800

    2139. 得到目标值的最少行动次数 (Minimum Moves to Reach Target Score)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -7385,19 +7385,14 @@ public class LeetCode_2 {
    // 2139. 得到目标值的最少行动次数 (Minimum Moves to Reach Target Score)
    public int minMoves(int target, int maxDoubles) {
       int res = 0;
-      while (target != 1) {
-         if ((target & 1) != 0) {
-            --target;
+      while (target > 1 && maxDoubles-- > 0) {
+         ++res;
+         if (target % 2 == 1) {
             ++res;
-         } else if (maxDoubles-- > 0) {
-            target >>>= 1;
-            ++res;
-         } else {
-            res += target - 1;
-            break;
          }
+         target /= 2;
       }
-      return res;
+      return res + target - 1;
 
    }
 
