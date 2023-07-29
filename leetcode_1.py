beefd9358cfb31ae76c376597bae0cd36fd5735b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-07-29 14:28:41 +0800

    2140. 解决智力问题 (Solving Questions With Brainpower)

--- leetcode_1.py
+++ leetcode_1.py
@@ -462,4 +462,15 @@ class leetcode_1 :
              return (dfs(i + 1, j) + dfs(i + 2, j) + dfs(i + 2, 1) + dfs(i + 2, 2)) % M
           return (dfs(i + 1, 0) + dfs(i + 1, 2 if j == 1 else 2)) % M
        return dfs(0, 0)
+    
+   # 2140. 解决智力问题 (Solving Questions With Brainpower) 
+    def mostPoints(self, questions: List[List[int]]) -> int:
+       n = len(questions)
+
+       @cache
+       def dfs(i: int) -> int:
+          if i >= n:
+             return 0
+          return max(dfs(i + 1), dfs(i + questions[i][1] + 1) + questions[i][0])
+       return dfs(0)
           
