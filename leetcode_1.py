1cc3ea11a29c4cf1d887f040a03f9f668cb4be25
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-08-03 11:42:22 +0800

    优化

--- leetcode_1.py
+++ leetcode_1.py
@@ -911,7 +911,7 @@ class leetcode_1 :
        dp = [0] * 26
        cnt = 0
        for i in range(n):
-          if i == 0 or (ord(s[i]) - ord(s[i - 1]) + 26) % 26 != 1:
+          if i == 0 or (ord(s[i]) - ord(s[i - 1])) % 26 != 1:
              cnt = 1
           else:
              cnt += 1
