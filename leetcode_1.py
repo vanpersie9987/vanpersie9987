3ad15f89c14460f7f88018efe574343fb4c6266e
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-08-10 11:56:25 +0800

    813. 最大平均值和的分组 (Largest Sum of Averages)

--- leetcode_1.py
+++ leetcode_1.py
@@ -1263,17 +1263,12 @@ class leetcode_1 :
     # 813. 最大平均值和的分组 (Largest Sum of Averages)
     def largestSumOfAverages(self, nums: List[int], k: int) -> float:
        n = len(nums)
-       pre = [0] * n
-       pre[-1] = nums[-1]
-       for i in range(n - 2, -1, -1):
-          pre[i] += pre[i + 1] + nums[i]
-
        @cache
        def dfs(i: int, j: int) -> float:
           if i == n:
              return 0
-          if j == 1:
-             return pre[i] / (n - i)
+          if j == 0:
+             return -inf
           sum = 0
           res = 0
           for x in range(i, n):
