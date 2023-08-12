1273d54095388adf8f877b9b73fded5875a20287
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-08-13 01:34:31 +0800

    88. 合并两个有序数组 (Merge Sorted Array)

--- leetcode_1.py
+++ leetcode_1.py
@@ -1565,3 +1565,23 @@ class leetcode_1 :
        for j in range(0, n):
           res = min(res, dfs(0, j))
        return res
+    
+    # 88. 合并两个有序数组 (Merge Sorted Array)
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+       p1 = m - 1
+       p2 = n - 1
+       p = m + n - 1
+       while p1 >= 0 and p2 >= 0:
+          if nums1[p1] > nums2[p2]:
+             nums1[p] = nums1[p1]
+             p1 -= 1
+          else:
+             nums1[p] = nums2[p2]
+             p2 -= 1
+          p -= 1
+       while p2 >= 0:
+          nums1[p] = nums2[p2]
+          p -= 1
+          p2 -= 1
+          
+
