31e511957d1a8f16d9b9f32265b6ff8bb2028620
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-08-14 16:25:37 +0800

    2100. 适合打劫银行的日子 (Find Good Days to Rob the Bank)

--- leetcode_1.py
+++ leetcode_1.py
@@ -1678,6 +1678,23 @@ class leetcode_1 :
                res %= MOD
              j = i
        return res if (cnt & 1) == 0 and cnt >= 2 else 0
+    
+    # 2100. 适合打劫银行的日子 (Find Good Days to Rob the Bank)
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        n = len(security)
+        left = [0] * n
+        for i in range(n - 2, -1, -1):
+            if security[i] <= security[i + 1]:
+                left[i] = left[i + 1] + 1
+        right = [0] * n
+        for i in range(1, n):
+            if security[i] <= security[i - 1]:
+                right[i] = right[i - 1] + 1
+        res = []
+        for i in range(time, n - time):
+            if left[i] >= time and right[i] >= time:
+                res.append(i)
+        return res
           
           
 
