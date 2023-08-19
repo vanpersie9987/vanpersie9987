31d88e68562883d1cd9130c6d108a4f92c06109e
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-08-19 20:42:12 +0800

    713. 乘积小于 K 的子数组 (Subarray Product Less Than K)

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -2875,14 +2875,4 @@ public class Leetcode_8 {
         }
         return res;
     }
-
-    public int sum(int num1, int num2) {
-        while (num2 != 0) {
-            int carry = (num1 & num2) << 1;
-            num1 ^= num2;
-            num2 = carry;
-        }
-        return num1;
-
-    }
 }
