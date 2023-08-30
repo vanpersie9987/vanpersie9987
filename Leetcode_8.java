4aed052f2d787c59763ccce22e7245c8e0113b3f
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-08-30 22:27:04 +0800

    contests/abc233

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -3158,4 +3158,47 @@ public class Leetcode_8 {
 
     }
 
+    /**
+     * 问题：
+     * https://atcoder.jp/contests/abc233/tasks/abc233_d
+     * 
+     * 输入 n(1≤n≤2e5) k(-1e15≤k≤1e15) 和长为 n 的数组 a(-1e9≤a[i]≤1e9)。
+     * 输出元素和等于 k 的连续子数组个数。
+     * 
+     * 如果你觉得本题太简单，请思考这个问题：
+     * 所有元素和等于 k 的连续子数组的长度之和。
+     */
+    /**
+     * 解答：
+     * 用前缀和思考：
+     * sum[R] - sum[L] = k
+     * 枚举 R，问题变成有多少个 sum[L]，也就是 sum[R]-k 的个数。
+     * 这可以用哈希表统计，代码如下。
+     * 
+     * https://atcoder.jp/contests/abc233/submissions/44903090
+     * 
+     * 关于思考题的提示：
+     * 举例：
+     * (R-L1) + (R-L2) + (R-L3)
+     * = 3*R - (L1+L2+L3)
+     * 所以除了维护前缀和的出现次数，还需要维护下标之和。
+     */
+    public int contests_abc233(int[] nums, int k) {
+        Map<Integer, int[]> map = new HashMap<>();
+        map.put(0, new int[] { 1, 0 });
+        int pre = 0;
+        int n = nums.length;
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            pre += nums[i];
+            int[] cur = map.getOrDefault(pre - k, new int[] { 0, 0 });
+            res += cur[0] * (i + 1) - cur[1];
+            int[] arr = map.getOrDefault(pre, new int[] { 0, 0 });
+            ++arr[0];
+            arr[1] -= i;
+            map.put(pre, arr);
+        }
+        return res;
+    }
+
 }
