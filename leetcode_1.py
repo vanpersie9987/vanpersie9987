616567ee67342b5e937f0625c74afb9a751c505d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-09-06 12:01:26 +0800

    1964. 找出到每个位置为止最长的有效障碍赛跑路线 (Find the Longest Valid Obstacle Course at Each Position)

--- leetcode_1.py
+++ leetcode_1.py
@@ -3808,6 +3808,7 @@ class leetcode_1 :
              list.append(v)
              res[i] = len(list)
           else:
+             # list中大于或等于 v + 1 的最小值的索引
              pos = bisect.bisect_left(list, v + 1)
              list[pos] = v
              res[i] = pos + 1
