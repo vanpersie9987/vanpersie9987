f9714a9f9b10eee02e3d27de1520bc1d63611163
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-09-06 17:02:37 +0800

    200. 岛屿数量 (Number of Islands)

--- leetcode_1.py
+++ leetcode_1.py
@@ -3861,3 +3861,24 @@ class leetcode_1 :
             dfs(root.left, d + 1)
         dfs(root, 0)
         return res
+    
+    # 200. 岛屿数量 (Number of Islands)
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        def dfs(i: int, j: int) -> None:
+            if i < 0 or i >= m or j < 0 or j >= n or not int(grid[i][j]):
+                return 
+            grid[i][j] = 0
+            dfs(i + 1, j)
+            dfs(i, j + 1)
+            dfs(i - 1, j)
+            dfs(i, j - 1)
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if int(grid[i][j]):
+                    res += 1
+                    dfs(i, j)
+        return res
