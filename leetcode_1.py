eeada42a40722f16f965a43543b7809d23413663
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-09-20 20:48:14 +0800

    面试题 08.01. 三步问题

--- leetcode_1.py
+++ leetcode_1.py
@@ -5049,6 +5049,18 @@ class leetcode_1 :
        res = []
        dfs(0, 0)
        return res
+    
+    # 面试题 08.01. 三步问题
+    def waysToStep(self, n: int) -> int:
+       a, b, c = 4, 2, 1
+       m = 10 ** 9 + 7
+       if n < 3:
+          return n
+       if n == 3:
+          return 4
+       for i in range(n - 3):
+          a, b, c = (a + b + c) % m, a, b
+       return a
 
 
 
