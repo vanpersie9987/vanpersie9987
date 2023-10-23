1b2d00bade0c0ab346e1d22f79a7dc5bb071e8a1
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-10-23 21:19:24 +0800

    P1831 杠杆数

--- luogu1.java
+++ luogu1.java
@@ -73,4 +73,61 @@ public class luogu1 {
         }
         return res;
     }
+
+    // P1831 杠杆数
+    public long leverNumber(long right, long left) {
+        return solve1831(right) - solve1831(left - 1);
+    }
+
+    private char[] arr1831;
+    private int n1831;
+    private long[][][] memo1831;
+
+    private long solve1831(long num) {
+        this.arr1831 = String.valueOf(num).toCharArray();
+        this.n1831 = arr1831.length;
+        this.memo1831 = new long[n1831][n1831][2000];
+        for (int i = 0; i < n1831; ++i) {
+            for (int j = 0; j < n1831; ++j) {
+                Arrays.fill(memo1831[i][j], -1L);
+            }
+        }
+        long res = 0L;
+        for (int i = 0; i < n1831; ++i) {
+            res += dfs1831(0, i, 0, true, false);
+        }
+        return res;
+    }
+
+    /**
+     * @param i       选择了第几位
+     * @param j       选择的杠杆位
+     * @param k       杠杆和
+     * @param isLimit 当前是否受限
+     * @param isNum   是否为数
+     * @return
+     */
+    private long dfs1831(int i, int j, int k, boolean isLimit, boolean isNum) {
+        if (i == n1831) {
+            return isNum && k == 0 ? 1L : 0L;
+        }
+        if (k < 0) {
+            return 0L;
+        }
+        if (!isLimit && isNum && memo1831[i][j][k] != -1L) {
+            return memo1831[i][j][k];
+        }
+        long res = 0L;
+        if (!isNum) {
+            res = dfs1831(i + 1, j, k, false, false);
+        }
+        int up = isLimit ? arr1831[i] - '0' : 9;
+        for (int d = isNum ? 0 : 1; d <= up; ++d) {
+            res += dfs1831(i + 1, j, k + (j - i) * d, isLimit && d == up, true);
+        }
+        if (!isLimit && isNum) {
+            memo1831[i][j][k] = res;
+        }
+        return res;
+    }
 }
