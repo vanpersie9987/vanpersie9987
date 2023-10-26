316bf5cf20bf672a28a873525495334e8afa4ecb
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-10-27 00:16:46 +0800

    优化

--- leetcode_1.py
+++ leetcode_1.py
@@ -7449,3 +7449,17 @@ class leetcode_1 :
                 res += 1
             copy //= 10
         return res
+    
+    # 1465. 切割后面积最大的蛋糕 (Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts)
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+       MOD = 10 ** 9 + 7
+       horizontalCuts.sort()
+       verticalCuts.sort()
+       max_h = max(horizontalCuts[0], h - horizontalCuts[-1])
+       for i in range(1, len(horizontalCuts)):
+          max_h = max(max_h, horizontalCuts[i] - horizontalCuts[i - 1])
+       max_w = max(verticalCuts[0], w - verticalCuts[-1])
+       for i in range(1, len(verticalCuts)):
+          max_w = max(max_w, verticalCuts[i] - verticalCuts[i - 1])
+       return max_h * max_w % MOD
+       
