c6d41400542693cf998b9adba1fc702bd5c36248
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-10-31 18:55:19 +0800

    2003. 每棵子树内缺失的最小基因值 (Smallest Missing Genetic Value in Each Subtree)

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -4892,4 +4892,46 @@ public class Leetcode_8 {
         return memo2920[i][j] = Math.max(res1, res2);
     }
 
+    // 2003. 每棵子树内缺失的最小基因值 (Smallest Missing Genetic Value in Each Subtree)
+    public int[] smallestMissingValueSubtree(int[] parents, int[] nums) {
+        int n = parents.length;
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
+        int node = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 1) {
+                node = i;
+            }
+        }
+        if (node < 0) {
+            return res;
+        }
+        List<Integer>[] g = new ArrayList[n];
+        Arrays.setAll(g, k -> new ArrayList<>());
+        for (int i = 1; i < n; ++i) {
+            g[parents[i]].add(i);
+        }
+        Set<Integer> vis = new HashSet<>();
+        int mex = 2;
+        while ((node >= 0)) {
+            dfs2003(node, g, vis, nums);
+            while (vis.contains(mex)) {
+                ++mex;
+            }
+            res[node] = mex;
+            node = parents[node];
+        }
+        return res;
+
+    }
+    
+    private void dfs2003(int x, List<Integer>[] g, Set<Integer> vis, int[] nums) {
+        vis.add(nums[x]); // 标记基因值
+        for (int son : g[x]) {
+            if (!vis.contains(nums[son])) {
+                dfs2003(son, g, vis, nums);
+            }
+        }
+    }
+
 }
