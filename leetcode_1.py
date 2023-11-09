551b948bea555432dfc05e42881da7882a7a759a
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-11-09 23:17:10 +0800

    2258. 逃离火灾 (Escape the Spreading Fire)

--- leetcode_1.py
+++ leetcode_1.py
@@ -42,6 +42,8 @@ from zoneinfo import reset_tzpath
 # pip3 install sortedcontainers
 from sortedcontainers import SortedList
 
+from symbol import return_stmt
+
 class leetcode_1 :
     class TreeNode:
        def __init__(self, val=0, left=None, right=None):
@@ -7862,9 +7864,143 @@ class leetcode_1 :
           i = j
           res = max(res, min(cnt0, cnt1) * 2)
        return res
+    
+    # 2258. 逃离火灾 (Escape the Spreading Fire)
+    def maximumMinutes(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
 
-           
+        # 返回能否在初始位置停留 t 分钟，并安全到达安全屋
+        def check(t: int) -> bool:
+            f = [(i, j) for i, row in enumerate(grid) for j, x in enumerate(row) if x == 1]
+            on_fire = set(f)  # 标记着火的位置
+            def spread_fire():
+                # 火的 BFS
+                nonlocal f
+                tmp = f
+                f = []
+                for i, j in tmp:
+                    for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):  # 上下左右
+                        if 0 <= x < m and 0 <= y < n and grid[x][y] == 0 and (x, y) not in on_fire:
+                            on_fire.add((x, y))  # 标记着火的位置
+                            f.append((x, y))
+            while t and f:  # 如果火无法扩散就提前退出
+                spread_fire()  # 火扩散
+                t -= 1
+            if (0, 0) in on_fire:
+                return False  # 起点着火，寄
+
+            # 人的 BFS
+            q = [(0, 0)]
+            vis = set(q)
+            while q:
+                tmp = q
+                q = []
+                for i, j in tmp:
+                    if (i, j) in on_fire: continue  # 人走到这个位置后，火也扩散到了这个位置
+                    for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):  # 上下左右
+                        if 0 <= x < m and 0 <= y < n and grid[x][y] == 0 and (x, y) not in on_fire and (x, y) not in vis:
+                            if x == m - 1 and y == n - 1:
+                                return True  # 我们安全了…暂时。
+                            vis.add((x, y))  # 避免反复访问同一个位置
+                            q.append((x, y))
+                spread_fire()  # 火扩散
+            return False  # 人被火烧到，或者没有可以到达安全屋的路
+
+        # 这里我用开区间二分（其它写法也可以）
+        left = -1
+        right = m * n + 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid
+        return left if left < m * n else 10 ** 9         return True
+                person.append((x, y))
+             for _ in range(size):
+                (x, y) = person.popleft()
+                for dx, dy in dirs:
+                   nx = x + dx
+                   ny = y + dy
+                   if nx == m - 1 and ny == n - 1 and not vis[nx][ny]:
+                      return True
+                   if m > nx >= 0 and n > ny >= 0 and not vis[nx][ny] and not fire[nx][ny] and grid[nx][ny] == 0:
+                      vis[nx][ny] = True
+                      person.append((nx, ny))
+             if q:
+                size = len(q)
+                for _ in range(size):
+                   (x, y) = q.popleft()
+                   for dx, dy in dirs:
+                      nx = x + dx
+                      ny = y + dy
+                      if m > nx >= 0 and n > ny >= 0 and not fire[nx][ny] and grid[nx][ny] == 0:
+                         fire[nx][ny] = True
+                         q.append((nx, ny))
+          return False
+       dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+       m = len(grid)
+       n = len(grid[0])
+       q = collections.deque()
+       fire = [[False] * n for _ in range(m)]
+       for i in range(m):
+          for j in range(n):
+             if grid[i][j] == 1:
+                q.append((i, j))
+                fire[i][j] = True
+       person = collections.deque()
+       person.append((0, 0))
+       vis = [[False] * n for _ in range(m)]
+       vis[0][0] = True
+       valid = False
+       while person:
+          size = len(person)
+          for _ in range(size):
+             (x, y) = person.popleft()
+             if x == m - 1 and y == n - 1:
+                valid = True
+                break
+             person.append((x, y))
+          if valid:
+             break
+          if q:
+             size = len(q)
+             for _ in range(size):
+                   (x, y) = q.popleft()
+                   for dx, dy in dirs:
+                      nx = x + dx
+                      ny = y + dy
+                      if m > nx >= 0 and n > ny >= 0 and not fire[nx][ny] and grid[nx][ny] == 0:
+                         fire[nx][ny] = True
+                         q.append((nx, ny))
+          size = len(person)
+          for _ in range(size):
+             (x, y) = person.popleft()
+             for dx, dy in dirs:
+                nx = x + dx
+                ny = y + dy
+                if not vis[nx][ny] and nx == m - 1 and ny == n - 1:
+                   valid = True
+                if m > nx >= 0 and n > ny >= 0 and not vis[nx][ny] and not fire[nx][ny] and grid[nx][ny] == 0:
+                   vis[nx][ny] = True
+                   person.append((nx, ny))
+       if not valid:
+          return -1
+       left = 0
+       right = m * n
+       res = 0
+       while left <= right:
+          mid = left + ((right - left) >> 1)
+          if check(mid):
+             res = mid
+             left = mid + 1
+          else:
+             right = mid - 1
+       return 10 ** 9 if res == m * n else res - 1
+
           
+                      
              
-                   
+
+
           
