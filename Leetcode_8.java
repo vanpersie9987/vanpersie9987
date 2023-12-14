2a41bbb8862fe386fc82e1b6a857a2852f6babf6
liumingyuan <mingyuan.liu@lingyue-digital.com>
2023-12-14 21:11:24 +0800

    2132. 用邮票贴满网格图 (Stamping the Grid)

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -5744,4 +5744,38 @@ public class Leetcode_8 {
         return res;
     }
 
+    // 2132. 用邮票贴满网格图 (Stamping the Grid)
+    public boolean possibleToStamp(int[][] grid, int stampHeight, int stampWidth) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] p = new int[m + 1][n + 1];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                p[i + 1][j + 1] = p[i][j + 1] + p[i + 1][j] - p[i][j] + grid[i][j];
+            }
+        }
+        int[][] d = new int[m + 2][n + 2];
+        for (int i2 = stampHeight; i2 <= m; ++i2) {
+            for (int j2 = stampWidth; j2 <= n; ++j2) {
+                int i1 = i2 - stampHeight + 1;
+                int j1 = j2 - stampWidth + 1;
+                if (p[i2][j2] - p[i2][j1 - 1] - p[i1 - 1][j2] + p[i1 - 1][j1 - 1] == 0) {
+                    ++d[i1][j1];
+                    --d[i1][j2 + 1];
+                    --d[i2 + 1][j1];
+                    ++d[i2 + 1][j2 + 1];
+                }
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                d[i + 1][j + 1] += d[i][j + 1] + d[i + 1][j] - d[i][j];
+                if (grid[i][j] == 0 && d[i + 1][j + 1] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
 }
