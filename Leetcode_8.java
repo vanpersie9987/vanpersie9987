522e34fe6943ca294a2ff1c99da5713d1f830ff3
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-02-05 10:11:39 +0800

    1696. 跳跃游戏 VI (Jump Game VI)

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -7197,27 +7197,4 @@ public class Leetcode_8 {
         }
     }
 
-    public int maxResult(int[] nums, int k) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        Deque<Integer> q = new ArrayDeque<>();
-        q.offerLast(0);
-        for (int i = 1; i < n; ++i) {
-            while (!q.isEmpty() && i - q.peekFirst() > k) {
-                q.pollFirst();
-            }
-            if (!q.isEmpty()) {
-                dp[i] = dp[q.peekFirst()] + nums[i];
-            }
-            while (!q.isEmpty() && dp[q.peekLast()] < dp[i]) {
-                q.pollLast();
-            }
-            q.offerLast(i);
-        }
-        return dp[n - 1];
-
-
-    }
-
 }
--- leetcode_1.py
+++ leetcode_1.py
@@ -9898,4 +9898,21 @@ class leetcode_1 :
                 break
              d[mask_y] = y
        return res
+
+    # 1696. 跳跃游戏 VI (Jump Game VI)
+    def maxResult(self, nums: List[int], k: int) -> int:
+       n = len(nums)
+       dp = [0] * n
+       dp[0] = nums[0]
+       q = collections.deque()
+       q.append(0)
+       for i in range(1, n):
+          while q and i - q[0] > k:
+             q.popleft()
+          if q:
+             dp[i] = dp[q[0]] + nums[i]
+          while q and dp[q[-1]] < dp[i]:
+             q.pop()
+          q.append(i)
+       return dp[-1]
           
