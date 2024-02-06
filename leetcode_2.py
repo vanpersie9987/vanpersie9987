ea0d52524c28c563c516d203f520d7de88a2bec4
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-02-06 15:05:49 +0800

    1499. 满足不等式的最大值 (Max Value of Equation)

--- leetcode_2.py
+++ leetcode_2.py
@@ -113,7 +113,22 @@ class leetcode_2:
                 q.pop()
             q.append(i)
         return max(dp)
-            
+    
+    # 1499. 满足不等式的最大值 (Max Value of Equation)
+    def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
+        q = deque()
+        res = -inf
+        for x, y in points:
+            while q and x - q[0][0] > k:
+                q.popleft()
+            if q:
+                res = max(res, x + y + q[0][1])
+            while q and q[-1][1] <= y - x:
+                q.pop()
+            q.append((x, y - x))
+        return res
+
+
 
 
 
