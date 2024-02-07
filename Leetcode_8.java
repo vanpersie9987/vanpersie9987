55ea7e878a69a7c29f8dec5320de3f77576bbe98
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-02-07 14:55:16 +0800

    LCR 184. 设计自助结算系统

--- Leetcode_8.java
+++ Leetcode_8.java
@@ -7359,4 +7359,42 @@ public class Leetcode_8 {
         return res;
     }
 
+    // LCR 184. 设计自助结算系统
+    class Checkout {
+        Queue<Integer> q;
+        Deque<Integer> d;
+
+        public Checkout() {
+            q = new ArrayDeque<>();
+            d = new ArrayDeque<>();
+
+        }
+
+        public int get_max() {
+            if (d.isEmpty()) {
+                return -1;
+            }
+            return d.peekFirst();
+        }
+
+        public void add(int value) {
+            while (!d.isEmpty() && d.peekLast() < value) {
+                d.pollLast();
+            }
+            d.offerLast(value);
+            q.offer(value);
+        }
+
+        public int remove() {
+            if (q.isEmpty()) {
+                return -1;
+            }
+            int res = q.poll();
+            if (res == d.peekFirst()) {
+                d.poll();
+            }
+            return res;
+        }
+    }
+
 }
