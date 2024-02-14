e0c88429bb68dd56e3cf698241ccd1c626933313
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-02-14 12:14:08 +0800

    2090. 半径为 k 的子数组平均值 (K Radius Subarray Averages)

--- leetcode_2.py
+++ leetcode_2.py
@@ -499,6 +499,23 @@ class leetcode_2:
             if i >= k - 1 and s // k >= threshold:
                 res += 1
         return res
+    
+    # 2090. 半径为 k 的子数组平均值 (K Radius Subarray Averages)
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        res = [-1] * n
+        s = 0
+        for i, v in enumerate(nums):
+            s += v
+            if i > 2 * k:
+                s -= nums[i - 2 * k - 1]
+            if i >= 2 * k:
+                res[i - k] = s // (2 * k + 1)
+        return res
+
+
+
+
 
 
 
