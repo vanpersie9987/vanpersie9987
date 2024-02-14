c063090a4b41cd155d55aba93536033f3d2f9b44
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-02-14 12:04:49 +0800

    1343. 大小为 K 且平均值大于等于阈值的子数组数目 (Number of Sub-arrays of Size K and Average    // Greater than or Equal to Threshold)

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -4424,23 +4424,17 @@ public class LeetCode_2 {
 
    }
 
-   // 1343. 大小为 K 且平均值大于等于阈值的子数组数目
-   // 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to
-   // Threshold
+   // 1343. 大小为 K 且平均值大于等于阈值的子数组数目 (Number of Sub-arrays of Size K and Average
+   // Greater than or Equal to Threshold)
    public int numOfSubarrays(int[] arr, int k, int threshold) {
-      int sum = k * threshold;
-      int cur = 0;
-      for (int i = 0; i < k; ++i) {
-         cur += arr[i];
-      }
       int res = 0;
-      if (cur >= sum) {
-         ++res;
-      }
-      for (int i = k; i < arr.length; ++i) {
-         cur -= arr[i - k];
-         cur += arr[i];
-         if (cur >= sum) {
+      int sum = 0;
+      for (int i = 0; i < arr.length; ++i) {
+         sum += arr[i];
+         if (i >= k) {
+            sum -= arr[i - k];
+         }
+         if (i >= k - 1 && sum / k >= threshold) {
             ++res;
          }
       }
