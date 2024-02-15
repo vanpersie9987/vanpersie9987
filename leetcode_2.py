70db052ae3a9ddc531afa450a3fade508c10643f
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-02-15 17:27:02 +0800

    1838. 最高频元素的频数 (Frequency of the Most Frequent Element)

--- leetcode_2.py
+++ leetcode_2.py
@@ -854,6 +854,24 @@ class leetcode_2:
             if s == k:
                 res = max(res, i - j + 1)
         return -1 if res == -1 else n - res
+    
+    # 1838. 最高频元素的频数 (Frequency of the Most Frequent Element)
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        j = 0
+        res = 1
+        s = 0
+        for i in range(1, len(nums)):
+            s += (nums[i] - nums[i - 1]) * (i - j)
+            while s > k:
+                s -= nums[i] - nums[j]
+                j += 1
+            res = max(res, i - j + 1)
+        return res
+
+            
+
+
 
 
 
