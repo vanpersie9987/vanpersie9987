325b808d90c341b33d3d1603b0aa5cf8a68d9f46
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-02-15 13:56:54 +0800

    1004. 最大连续1的个数 III (Max Consecutive Ones III)

--- leetcode_2.py
+++ leetcode_2.py
@@ -795,6 +795,19 @@ class leetcode_2:
         arr1 = [1 if c == 'T' else 0 for c in answerKey]
         arr2 = [1 if c == 'F' else 0 for c in answerKey]
         return max(check(arr1), check(arr2))
+    
+    # 1004. 最大连续1的个数 III (Max Consecutive Ones III)
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        res = 0
+        cnt0 = 0
+        j = 0
+        for i, v in enumerate(nums):
+            cnt0 += 1 ^ v
+            while cnt0 > k:
+                cnt0 -= 1 ^ nums[j]
+                j += 1
+            res = max(res, i - j + 1)
+        return res
             
 
 
