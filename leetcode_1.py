7c68488cb65a3aeb4db128533236303948e4cc5d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-04-23 11:51:48 +0800

    1994. 好子集的数目 (The Number of Good Subsets)

--- leetcode_1.py
+++ leetcode_1.py
@@ -6419,37 +6419,40 @@ class leetcode_1:
 
     # 1994. 好子集的数目 (The Number of Good Subsets)
     def numberOfGoodSubsets(self, nums: List[int]) -> int:
-        MOD = 10**9 + 7
-
         @cache
         def dfs(i: int, j: int) -> int:
-            if i == 31:
-                return 1 if j > 1 else 0
+            if i == len(arr):
+                return 1
             res = dfs(i + 1, j)
-            if i == 1:
-                res += (pow(2, cnts[i], MOD) - 1) * dfs(i + 1, j | 1)
-            elif masks[i] != -1 and (masks[i] & j) == 0:
-                res += cnts[i] * dfs(i + 1, masks[i] | j)
+            if j & dic[arr[i]] == 0:
+                res += dfs(i + 1, j | dic[arr[i]]) * cnts[arr[i]] % MOD
             return res % MOD
-
-        primes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-        dic = collections.defaultdict(int)
-        for i, p in enumerate(primes):
-            dic[p] = i
-        cnts = [0] * 31
-        for num in nums:
-            cnts[num] += 1
-        masks = [0] * 31
-        for i in range(1, 31):
-            if i in dic.keys():
-                masks[i] = 1 << dic[i]
-            elif i % 4 == 0 or i % 9 == 0 or i % 25 == 0:
-                masks[i] = -1
-            else:
-                for j in range(2, i + 1):
-                    if j in dic.keys() and i % j == 0:
-                        masks[i] |= 1 << dic[j]
-        return dfs(1, 0)
+        def check(x: int) -> int:
+            m = 0
+            for i in range(2, isqrt(x) + 1):
+                while x % i == 0:
+                    if m >> i & 1 == 1:
+                        return 0
+                    m |= 1 << i
+                    x //= i
+            if x > 1:
+                m |= 1 << x
+            return m
+        dic = defaultdict(int)
+        for i in range(2, 31):
+            x = check(i)
+            if x:
+                dic[i] = x
+        cnts = defaultdict(int)
+        cnt1 = 0
+        for x in nums:
+            if x == 1:
+                cnt1 += 1
+            elif x in dic:
+                cnts[x] += 1
+        arr = list(cnts.keys())
+        MOD = 10**9 + 7
+        return (dfs(0, 0) - 1) * pow(2, cnt1, MOD) % MOD
 
     # 101. 对称二叉树 (Symmetric Tree)
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
