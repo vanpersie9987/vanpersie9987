15d6958a67353604a0a1ec9e1e3877806bea8900
liumingyuan <mingyuan.liu@lingyue-digital.com>
2024-05-11 19:19:40 +0800

    3008. 找出数组中的美丽下标 II (Find Beautiful Indices in the Given Array II)

--- Leetcode_9.java
+++ Leetcode_9.java
@@ -56,4 +56,45 @@ public class Leetcode_9 {
             this.right = right;
         }
     }
+
+    // 3008. 找出数组中的美丽下标 II (Find Beautiful Indices in the Given Array II)
+    public List<Integer> beautifulIndices(String s, String a, String b, int k) {
+        List<Integer> aList = zAlgorithm(a, s);
+        List<Integer> bList = zAlgorithm(b, s);
+        List<Integer> res = new ArrayList<>();
+        int j = 0;
+        for (int x : aList) {
+            while (j < bList.size() && x - bList.get(j) > k) {
+                ++j;
+            }
+            if (j < bList.size() && Math.abs(x - bList.get(j)) <= k) {
+                res.add(x);
+            }
+        }
+        return res;
+
+    }
+
+    private List<Integer> zAlgorithm(String t, String s) {
+        String ss = t + s;
+        int n = ss.length();
+        int[] z = new int[n];
+        int left = 0;
+        int right = 0;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i < n; ++i) {
+            if (i <= right) {
+                z[i] = Math.min(z[i - left], right - i + 1);
+            }
+            while (i + z[i] < n && ss.charAt(z[i]) == ss.charAt(i + z[i])) {
+                left = i;
+                right = i + z[i];
+                ++z[i];
+            }
+            if (i >= t.length() && z[i] >= t.length()) {
+                res.add(i - t.length());
+            }
+        }
+        return res;
+    }
 }
