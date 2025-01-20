db903c5ccc4efde7aa496e6b4493cd388df3f096
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-01-20 10:19:36 +0800

    3423. 循环数组中相邻元素的最大差值 (Maximum Difference Between Adjacent Elements in a     // Circular Array)

--- Leetcode_9.java
+++ Leetcode_9.java
@@ -4252,4 +4252,17 @@ public class Leetcode_9 {
         return res;
     }
 
+    // 3423. 循环数组中相邻元素的最大差值 (Maximum Difference Between Adjacent Elements in a
+    // Circular Array)
+    public int maxAdjacentDistance(int[] nums) {
+        int n = nums.length;
+        int res = Math.abs(nums[0] - nums[n - 1]);
+        for (int i = 1; i < n; ++i) {
+            res = Math.max(res, Math.abs(nums[i] - nums[i - 1]));
+        }
+        return res;
+
+
+    }
+
 }
