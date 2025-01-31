44d7f7a4ac24d736b5dfeffb96440d8cabfc6b66
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-01-31 17:41:45 +0800

    541. 反转字符串 II (Reverse String II)

--- Leetcode_9.java
+++ Leetcode_9.java
@@ -4414,4 +4414,22 @@ public class Leetcode_9 {
         lastDepth3425.put(color, lastDep);
     }
 
+    // 541. 反转字符串 II (Reverse String II)
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        for (int i = 0; i < n; i += 2 * k) {
+            int l = i;
+            int r = Math.min(i + k - 1, n - 1);
+            while (l < r) {
+                char tmp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = tmp;
+                ++l;
+                --r;
+            }
+        }
+        return new String(arr);
+    }
+
 }
