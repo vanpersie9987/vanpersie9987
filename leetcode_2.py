95a75667c5a5f015ca3be654b9eb5252df44ea0b
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-04-14 10:12:57 +0800

    1534. 统计好三元组 (ount Good Triplets)

--- leetcode_2.py
+++ leetcode_2.py
@@ -9458,3 +9458,14 @@ class Union924:
             n = len(s)
             return dfs(0, 0, True, False)
         return (cal(int(r)) - cal(int(l) - 1)) % mod
+
+    # 1534. 统计好三元组 (ount Good Triplets)
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        res = 0
+        n = len(arr)
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        res += 1
+        return res
