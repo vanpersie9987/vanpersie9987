ebf9c7da598924e3fb84636644585e53dc51ba67
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-06-21 16:26:29 +0800

    3439. 重新安排会议得到最多空余时间 I (Reschedule Meetings for Maximum Free Time I)

--- Leetcode_9.java
+++ Leetcode_9.java
@@ -7059,4 +7059,27 @@ public class Leetcode_9 {
 
     }
 
+    // 3439. 重新安排会议得到最多空余时间 I (Reschedule Meetings for Maximum Free Time I)
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int n = startTime.length;
+        int[] free = new int[n + 1];
+        free[0] = startTime[0];
+        for (int i = 1; i < startTime.length; ++i) {
+            free[i] = startTime[i] - endTime[i - 1];
+        }
+        free[n] = eventTime - endTime[n - 1];
+        int res = 0;
+        int cur = 0;
+        for (int i = 0; i < n + 1; ++i) {
+            cur += free[i];
+            if (i < k) {
+                continue;
+            }
+            res = Math.max(res, cur);
+            cur -= free[i - k];
+        }
+        return res;
+
+    }
+
 }
--- leetcode_3.py
+++ leetcode_3.py
@@ -1075,3 +1075,22 @@ class leetcode_3:
             if x != y and cnts[x] == x and cnts[y] == y:
                 return f"{x}{y}"
         return ""
+    
+    # 3439. 重新安排会议得到最多空余时间 I (Reschedule Meetings for Maximum Free Time I)
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        n = len(startTime)
+        free = [0] * (n + 1)
+        free[0] = startTime[0]
+        for i in range(1, n):
+            free[i] = startTime[i] - endTime[i - 1]
+        free[n] = eventTime - endTime[-1]
+        res = 0
+        s = 0
+        for i, v in enumerate(free):
+            s += v
+            if i < k:
+                continue
+            res = max(res, s)
+            s -= free[i - k]
+        return res
+    
