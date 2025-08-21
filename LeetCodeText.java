d9177bcc8458da79d6aa88c4f043e4ffa26804ed
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-08-21 09:35:08 +0800

    1504. 统计全 1 子矩形 (Count Submatrices With All Ones) --单调栈

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -6209,6 +6209,37 @@ public class LeetCodeText {
 
     }
 
+    // 1504. 统计全 1 子矩形 (Count Submatrices With All Ones) --单调栈
+    public int numSubmat2(int[][] mat) {
+        int n = mat[0].length;
+        int[] height = new int[n];
+        int res = 0;
+        for (int[] row : mat) {
+            for (int j = 0; j < n; ++j) {
+                if (row[j] == 0) {
+                    height[j] = 0;
+                } else {
+                    height[j] += 1;
+                }
+            }
+            Stack<int[]> st = new Stack<>();
+            // (j, f, heights[j])
+            st.add(new int[] { -1, 0, -1 });
+            for (int j = 0; j < n; ++j) {
+                while (st.peek()[2] >= height[j]) {
+                    st.pop();
+                }
+                int left = st.peek()[0];
+                int f = st.peek()[1];
+                f += (j - left) * height[j];
+                res += f;
+                st.add(new int[] { j, f, height[j] });
+            }
+        }
+        return res;
+
+    }
+
     public List<Integer> pancakeSort(final int[] A) {
         List<Integer> result = new ArrayList<>();
         int temp = 0;
