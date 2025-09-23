12790c5d7ef5a30c9d015f4e4517452dc78629bf
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-09-23 10:14:11 +0800

    3688. 偶数的按位或运算 (Bitwise OR of Even Numbers in an Array)

--- Leetcode_10.java
+++ Leetcode_10.java
@@ -8,4 +8,16 @@ import java.util.TreeSet;
 
 public class Leetcode_10 {
 
+    // 3688. 偶数的按位或运算 (Bitwise OR of Even Numbers in an Array)
+    public int evenNumberBitwiseORs(int[] nums) {
+        int res = 0;
+        for (int x : nums) {
+            if ((x & 1) == 0) {
+                res |= x;
+            }
+        }
+        return res;
+
+    }
+
 }
