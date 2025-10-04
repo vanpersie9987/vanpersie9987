63d7057d7de669143a4377ac3123f17f066945cf
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-10-04 08:49:48 +0800

    11. 盛最多水的容器 (Container With Most Water)

--- leetcode_3.py
+++ leetcode_3.py
@@ -4191,3 +4191,15 @@ class SegmentTree2940:
                     heapq.heappush(q, (max(h, heightMap[nx][ny]), nx, ny))
                     heightMap[nx][ny] = -1
         return res
+
+    # 11. 盛最多水的容器 (Container With Most Water)
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        res = 0
+        while l < r:
+            res = max(res, (r - l) * min(height[l], height[r]))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
