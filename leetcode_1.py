0bce20af9f7e63a2a4a94508502c600a6a307533
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-10-22 16:03:49 +0800

    88. 合并两个有序数组 (Merge Sorted Array)

--- leetcode_1.py
+++ leetcode_1.py
@@ -1638,10 +1638,7 @@ class leetcode_1:
         p = m + n - 1
         p1 = m - 1
         p2 = n - 1
-        while p2 >= 0:
-            if p1 < 0:
-                nums1[:p2 + 1] = nums2[:p2 + 1]
-                break
+        while p1 >= 0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
                 p1 -= 1
@@ -1649,6 +1646,7 @@ class leetcode_1:
                 nums1[p] = nums2[p2]
                 p2 -= 1
             p -= 1
+        nums1[:p2 + 1] = nums2[:p2 + 1]
 
     # 2320. 统计放置房子的方式数 (Count Number of Ways to Place Houses)
     def countHousePlacements(self, n: int) -> int:
