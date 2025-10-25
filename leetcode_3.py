68e91710e3d8025cb268f7ed5810b8f980238903
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-10-26 00:18:46 +0800

    2043. 简易银行系统 (Simple Bank System)

--- leetcode_3.py
+++ leetcode_3.py
@@ -4780,24 +4780,26 @@ class SegmentTree2940:
             self.n = len(balance)
 
         def transfer(self, account1: int, account2: int, money: int) -> bool:
-            if account1 < 1 or account1 > self.n or account2 < 1 or account2 > self.n:
-                return False
-            if self.balance[account1 - 1] < money:
-                return False
-            self.balance[account1 - 1] -= money
-            self.balance[account2 - 1] += money
-            return True
+            return (
+                self.accountIsLegal(account1)
+                and self.accountIsLegal(account2)
+                and self.withdraw(account1, money)
+                and self.deposit(account2, money)
+            )
 
         def deposit(self, account: int, money: int) -> bool:
-            if account < 1 or account > self.n:
+            if not self.accountIsLegal(account):
                 return False
             self.balance[account - 1] += money
             return True
 
         def withdraw(self, account: int, money: int) -> bool:
-            if account < 1 or account > self.n:
+            if not self.accountIsLegal(account):
                 return False
             if self.balance[account - 1] < money:
                 return False
             self.balance[account - 1] -= money
             return True
+
+        def accountIsLegal(self, account: int) -> bool:
+            return 1 <= account <= self.n
