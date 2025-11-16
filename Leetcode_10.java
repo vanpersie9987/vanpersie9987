78ca11e51a05b3ed66b9d827c332fe1466f2e8da
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-11-16 17:11:44 +0800

    3745. 三元素表达式的最大值 (Maximize Expression of Three Elements)

--- Leetcode_10.java
+++ Leetcode_10.java
@@ -1411,4 +1411,24 @@ public class Leetcode_10 {
         }
         return res;
     }
+
+    // 3745. 三元素表达式的最大值 (Maximize Expression of Three Elements)
+    public int maximizeExpressionOfThree(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        for (int x : nums) {
+            if (x >= max1) {
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max2 = x;
+            }
+            if (x < min1) {
+                min1 = x;
+            }
+        }
+        return max1 + max2 - min1;
+
+    }
 }
