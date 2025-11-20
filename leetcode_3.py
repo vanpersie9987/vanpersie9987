09f0dc68055b877839a05590fc4a942bd3a66743
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-11-20 09:50:04 +0800

    757. 设置交集大小至少为2 (Set Intersection Size At Least Two)

--- leetcode_3.py
+++ leetcode_3.py
@@ -5411,3 +5411,19 @@ class SegmentTree2940:
             s += k
             res += s * d
         return res
+    
+    # 757. 设置交集大小至少为2 (Set Intersection Size At Least Two)
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[1], -x[0]))
+        s = e = -1
+        ans = 0
+        for a, b in intervals:
+            if a <= s:
+                continue
+            if a > e:
+                ans += 2
+                s, e = b - 1, b
+            else:
+                ans += 1
+                s, e = e, b
+        return ans
