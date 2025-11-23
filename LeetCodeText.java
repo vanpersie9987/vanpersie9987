01d6caf3aae59c60fb30d71227820f44d3190d96
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-11-24 00:16:55 +0800

    1018. 可被 5 整除的二进制前缀 (Binary Prefix Divisible By 5)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -3921,16 +3921,15 @@ public class LeetCodeText {
 
     }
 
-    // 1018. 可被 5 整除的二进制前缀
-    public List<Boolean> prefixesDivBy5(final int[] A) {
-        List<Boolean> result = new ArrayList<>();
-        int num = 0;
-        for (int i = 0; i < A.length; ++i) {
-            num = ((num << 1) + A[i]) % 5;
-            result.add(num == 0);
+    // 1018. 可被 5 整除的二进制前缀 (Binary Prefix Divisible By 5)
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> res = new ArrayList<>();
+        int v = 0;
+        for (int x : nums) {
+            v = ((v << 1) + x) % 5;
+            res.add(v == 0);
         }
-        return result;
-
+        return res;
     }
 
     public int game(final int[] guess, final int[] answer) {
