14fd3d6db19698e5b54afd93d04684eccf5f3afa
liumingyuan <mingyuan.liu@lingyue-digital.com>
2025-12-29 11:38:13 +0800

    3790. 最小全 1 倍数 (mallest All-Ones Multiple)

--- leetcode_3.py
+++ leetcode_3.py
@@ -6317,3 +6317,14 @@ class SegmentTree2940:
         need2 -= m
         res3 += need1 * cost1 + need2 * cost2
         return min(res1, res2, res3)
+
+    # 3790. 最小全 1 倍数 (mallest All-Ones Multiple)
+    def minAllOneMultiple(self, k: int) -> int:
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
+        rem = 0
+        for i in range(1, k + 1):
+            rem = (rem * 10 + 1) % k
+            if rem == 0:
+                return i
+        return -1
