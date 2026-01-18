69e681874552266f925130786473e1cb7c4bbd59
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-01-19 00:28:08 +0800

    1292. 元素和小于等于阈值的正方形的最大边长 (Maximum Side Length of a Square with Sum Less than or Equal to Threshold)

--- leetcode_3.py
+++ leetcode_3.py
@@ -6949,7 +6949,9 @@ class SegmentTree2940:
         return res
 
     # 3812. 翻转树上最少边 (Minimum Edge Toggles on a Tree)
-    def minimumFlips(self, n: int, edges: List[List[int]], start: str, target: str) -> List[int]:
+    def minimumFlips(
+        self, n: int, edges: List[List[int]], start: str, target: str
+    ) -> List[int]:
         def dfs(x: int, fa: int) -> int:
             cnt = int(start[x] != target[x])
             for y, id in g[x]:
@@ -6959,6 +6961,7 @@ class SegmentTree2940:
                         res.append(id)
                     cnt += cnt_y
             return cnt & 1
+
         if start.count("1") % 2 != target.count("1") % 2:
             return [-1]
         g = [[] for _ in range(n)]
@@ -6968,3 +6971,32 @@ class SegmentTree2940:
         res = []
         dfs(0, -1)
         return sorted(res)
+
+    # 1292. 元素和小于等于阈值的正方形的最大边长 (Maximum Side Length of a Square with Sum Less than or Equal to Threshold)
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        m, n = len(mat), len(mat[0])
+        pre = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                pre[i + 1][j + 1] = (
+                    pre[i][j + 1] + pre[i + 1][j] - pre[i][j] + mat[i][j]
+                )
+
+        def check(k: int) -> bool:
+            for i in range(m - k + 1):
+                for j in range(n - k + 1):
+                    total = (
+                        pre[i + k][j + k] - pre[i][j + k] - pre[i + k][j] + pre[i][j]
+                    )
+                    if total <= threshold:
+                        return True
+            return False
+
+        left, right = 0, min(m, n)
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            if check(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left - 1
