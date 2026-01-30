6306869cea7b9cfa01e2bd769f670730eada456e
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-01-31 00:11:22 +0800

    744. 寻找比目标字母大的最小字母 (Find Smallest Letter Greater Than Target)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -9177,24 +9177,19 @@ public class LeetCodeText {
         return false;
     }
 
-    // 744. 寻找比目标字母大的最小字母
+    // 744. 寻找比目标字母大的最小字母 (Find Smallest Letter Greater Than Target)
     public char nextGreatestLetter(char[] letters, char target) {
-        if (target < letters[0] || target >= letters[letters.length - 1]) {
-            return letters[0];
-        }
-        char ans = 'a';
         int left = 0;
         int right = letters.length - 1;
         while (left <= right) {
-            int mid = left + ((right - left) >>> 1);
+            int mid = left + ((right - left) >> 1);
             if (letters[mid] > target) {
-                ans = letters[mid];
                 right = mid - 1;
-            } else if (letters[mid] <= target) {
+            } else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return letters[(right + 1) % letters.length];
 
     }
 
