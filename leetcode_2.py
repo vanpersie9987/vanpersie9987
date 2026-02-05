6bcb0afb0f5d2a2b0160650c5f8c459dea758f8d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-02-05 09:43:17 +0800

    3379. 转换数组 (Transformed Array)

--- leetcode_2.py
+++ leetcode_2.py
@@ -7716,11 +7716,7 @@ class Union924:
 
     # 3379. 转换数组 (Transformed Array)
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = []
-        for i, x in enumerate(nums):
-            res.append(nums[(i + x) % n])
-        return res
+        return [nums[(i + x) % len(nums)] for i, x in enumerate(nums)]
 
     # 3381. 长度可被 K 整除的子数组的最大元素和 (Maximum Subarray Sum With Length Divisible by K)
     def maxSubarraySum(self, nums: List[int], k: int) -> int:
