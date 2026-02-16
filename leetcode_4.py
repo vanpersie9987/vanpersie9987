a209938b114163c38cf81842f156f89572fd7097
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-02-16 20:47:58 +0800

    682. 棒球比赛 (Baseball Game)

--- leetcode_4.py
+++ leetcode_4.py
@@ -280,3 +280,17 @@ class leetcode_4:
     # 709. 转换成小写字母 (To Lower Case)
     def toLowerCase(self, s: str) -> str:
         return s.lower()
+
+    # 682. 棒球比赛 (Baseball Game)
+    def calPoints(self, operations: List[str]) -> int:
+        st = []
+        for op in operations:
+            if op == "+":
+                st.append(st[-1] + st[-2])
+            elif op == "D":
+                st.append(st[-1] * 2)
+            elif op == "C":
+                st.pop()
+            else:
+                st.append(int(op))
+        return sum(st)
