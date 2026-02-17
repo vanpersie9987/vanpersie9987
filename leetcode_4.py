d4808a40d371b8e0696d14863e7bd5c3b68fb6c8
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-02-17 20:14:23 +0800

    3553. 包含要求路径的最小带权子图 II (Minimum Weighted Subgraph With the Required Paths II) --LCA 最近公共祖先

--- leetcode_4.py
+++ leetcode_4.py
@@ -371,7 +371,7 @@ class leetcode_4:
         return res
 
 
-# 最近公共祖先（LCA）、倍增算法 带权树 LCA 模板（节点编号从 0 开始）：
+# 最近公共祖先（LCA）、倍增算法 边权为1 LCA 模板（节点编号从 0 开始）：
 class LcaBinaryLifting:
     def __init__(self, edges: List[List[int]]):
         n = len(edges) + 1
@@ -449,3 +449,78 @@ class LcaBinaryLifting:
             if d:
                 res[i] = pow2[d - 1]
         return res
+
+
+# 最近公共祖先（LCA）、倍增算法 带权树 LCA 模板（节点编号从 0 开始）：
+class LcaBinaryLifting:
+    def __init__(self, edges: List[List[int]]):
+        n = len(edges) + 1
+        m = n.bit_length()
+        g = [[] for _ in range(n)]
+        for x, y, w in edges:
+            # 如果题目的节点编号从 1 开始，改成 x-1 和 y-1
+            g[x].append((y, w))
+            g[y].append((x, w))
+
+        depth = [0] * n
+        dis = [0] * n  # 如果是无权树（边权为 1），dis 可以去掉，用 depth 代替
+        pa = [[-1] * n for _ in range(m)]
+
+        def dfs(x: int, fa: int) -> None:
+            pa[0][x] = fa
+            for y, w in g[x]:
+                if y != fa:
+                    depth[y] = depth[x] + 1
+                    dis[y] = dis[x] + w
+                    dfs(y, x)
+
+        dfs(0, -1)
+
+        for i in range(m - 1):
+            for x in range(n):
+                if (p := pa[i][x]) != -1:
+                    pa[i + 1][x] = pa[i][p]
+
+        self.depth = depth
+        self.dis = dis
+        self.pa = pa
+
+    # 返回 node 的第 k 个祖先节点
+    # 如果不存在，返回 -1
+    def get_kth_ancestor(self, node: int, k: int) -> int:
+        pa = self.pa
+        for i in range(k.bit_length()):
+            if k >> i & 1:
+                node = pa[i][node]
+                if node < 0:
+                    return -1
+        return node
+
+    # 返回 x 和 y 的最近公共祖先
+    def get_lca(self, x: int, y: int) -> int:
+        if self.depth[x] > self.depth[y]:
+            x, y = y, x
+        # 使 y 和 x 在同一深度
+        y = self.get_kth_ancestor(y, self.depth[y] - self.depth[x])
+        if y == x:
+            return x
+        pa = self.pa
+        for i in range(len(pa) - 1, -1, -1):
+            px, py = pa[i][x], pa[i][y]
+            if px != py:
+                x, y = px, py  # 同时往上跳 2**i 步
+        return pa[0][x]
+
+    # 返回 x 到 y 的距离（最短路长度）
+    def get_dis(self, x: int, y: int) -> int:
+        return self.dis[x] + self.dis[y] - self.dis[self.get_lca(x, y)] * 2
+
+    # 3553. 包含要求路径的最小带权子图 II (Minimum Weighted Subgraph With the Required Paths II) --LCA 最近公共祖先
+    def minimumWeight(
+        self, edges: List[List[int]], queries: List[List[int]]
+    ) -> List[int]:
+        lca = self.LcaBinaryLifting(edges)
+        return [
+            (lca.get_dis(a, b) + lca.get_dis(b, c) + lca.get_dis(c, a)) // 2
+            for a, b, c in queries
+        ]
