78718b8abfe9bfaefdcf55e7edf85ac2df21554d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-02-20 09:49:16 +0800

    669. 修剪二叉搜索树 (Trim a Binary Search Tree)

--- leetcode_4.py
+++ leetcode_4.py
@@ -687,3 +687,15 @@ class LcaBinaryLifting:
                     left = i + 1
         res.sort(reverse=True)
         return "".join(res)
+
+    # 669. 修剪二叉搜索树 (Trim a Binary Search Tree)
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        if root.val > high:
+            return self.trimBST(root.left, low, high)
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+        return root
