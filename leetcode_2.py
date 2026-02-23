cc2da180863a562a2e47e2347ecba4a8c4bb16ef
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-02-23 10:20:29 +0800

    917. 仅仅反转字母 (Reverse Only Letters)

--- leetcode_2.py
+++ leetcode_2.py
@@ -10075,3 +10075,18 @@ class Union924:
         if n == len(edges):  # 环
             ans += 2
         return ans
+
+    # 917. 仅仅反转字母 (Reverse Only Letters)
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = [x for x in s]
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalpha():
+                i += 1
+            while i < j and not s[j].isalpha():
+                j -= 1
+            if i < j:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+        return ''.join(s)
