e7271b1ceebb8860e480287efafe0f6752b1225c
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-03-26 15:14:27 +0800

    2139. 得到目标值的最少行动次数 (Minimum Moves to Reach Target Score)

--- leetcode_4.py
+++ leetcode_4.py
@@ -2322,3 +2322,19 @@ class LcaBinaryLifting:
         for i, q in enumerate(queries):
             queries[i] = total[q]
         return queries
+
+    # 2139. 得到目标值的最少行动次数 (Minimum Moves to Reach Target Score)
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        res = 0
+        while target != 1:
+            if maxDoubles == 0:
+                res += target - 1
+                break
+            if target & 1:
+                res += 1
+                target -= 1
+            else:
+                res += 1
+                target >>= 1
+                maxDoubles -= 1
+        return res
