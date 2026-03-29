8cb96562eee2528e2bd81316bf0e73f9c4488ab7
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-03-29 15:18:03 +0800

    3884. 双端字符匹配 (First Matching Character From Both Ends)

--- leetcode_4.py
+++ leetcode_4.py
@@ -2415,3 +2415,11 @@ class LcaBinaryLifting:
                 j -= 1
             f = g
         return sum(f) % MOD
+    
+    # 3884. 双端字符匹配 (First Matching Character From Both Ends)
+    def firstMatchingIndex(self, s: str) -> int:
+        n = len(s)
+        for i in range((n + 1) // 2):
+            if s[i] == s[n - 1 - i]:
+                return i
+        return -1
