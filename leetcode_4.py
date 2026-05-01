41c2a55e4a62d61564e800cfbdca0f1fcdd12d45
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-05-01 09:35:05 +0800

    396. 旋转函数 (Rotate Function)

--- leetcode_4.py
+++ leetcode_4.py
@@ -3278,3 +3278,20 @@ class LcaBinaryLifting:
             _a2 = ((s - pre_s) - (len(a) - i - 1) * v) // x
             res = min(res, _a1 + _a2)
         return res
+    
+    # 396. 旋转函数 (Rotate Function)
+    def maxRotateFunction(self, nums: list[int]) -> int:
+        n = len(nums)
+        f = sum(i * x for i, x in enumerate(nums))  # F(0)
+        s = sum(nums)  # nums 的总和
+
+        ans = f
+        for i in range(n - 1, 0, -1):
+            f += s - n * nums[i]
+            ans = max(ans, f)
+        return ans
+        
+            
+
+
+
