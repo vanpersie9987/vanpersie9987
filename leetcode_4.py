5314d24815a943536da12d35074657f3e22eadf0
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-05-05 09:13:02 +0800

    3918. 区间内的质数和 (Sum of Primes Between Number and Its Reverse)

--- leetcode_4.py
+++ leetcode_4.py
@@ -3333,14 +3333,10 @@ class LcaBinaryLifting:
     # 3918. 区间内的质数和 (Sum of Primes Between Number and Its Reverse)
     def sumOfPrimesInRange(self, n: int) -> int:
         def is_prime(x: int) -> bool:
-            if x == 1:
-                return False
-            if x == 2:
-                return True
             for i in range(2, isqrt(x) + 1):
                 if x % i == 0:
                     return False
-            return True
+            return x >= 2
 
         def rev(x: int) -> int:
             res = 0
