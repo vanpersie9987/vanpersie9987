d9b9841e2bff8946fe6e9321677c56e0d591152d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-05-15 10:21:10 +0800

    153. 寻找旋转排序数组中的最小值 (Find Minimum in Rotated Sorted Array)

--- LeetCodeText.java
+++ LeetCodeText.java
@@ -1601,22 +1601,21 @@ public class LeetCodeText {
         return memo152[i][j] = res;
     }
 
-    // 153. 寻找旋转排序数组中的最小值
-    public int findMin(final int[] nums) {
+    // 153. 寻找旋转排序数组中的最小值 (Find Minimum in Rotated Sorted Array)
+    public int findMin(int[] nums) {
+        int n = nums.length;
         int left = 0;
-        int right = nums.length - 1;
+        int right = n - 1;
+        int x = nums[n - 1];
         while (left <= right) {
-            int mid = left + ((right - left) >>> 1);
-            if (mid + 1 < nums.length && nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
-            }
-            if (nums[left] > nums[mid]) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < x) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return nums[0];
+        return nums[Math.min(right + 1, n - 1)];
 
     }
 
