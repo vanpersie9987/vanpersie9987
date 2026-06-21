8312bc8b62f08c3959a0f59cf1e49f3259a0cdba
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-06-22 00:09:38 +0800

    1189. “气球” 的最大数量 (Maximum Number of Balloons)

--- leetcode_4.py
+++ leetcode_4.py
@@ -4314,3 +4314,19 @@ class LcaBinaryLifting:
         for u, v in edges:
             g[u].append(v)
         return dfs(0)
+
+    # 1189. “气球” 的最大数量 (Maximum Number of Balloons)
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b, a, l, o, n = 0, 0, 0, 0, 0
+        for x in text:
+            if x == "b":
+                b += 1
+            elif x == "a":
+                a += 1
+            elif x == "l":
+                l += 1
+            elif x == "o":
+                o += 1
+            elif x == "n":
+                n += 1
+        return min(b, a, l // 2, o // 2, n)
