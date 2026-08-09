b36ed9cfafb32e52fe555c04c045da344dd6c2cd
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-08-09 12:42:03 +0800

    应用折扣后的最低总价

--- Leetcode_11.java
+++ Leetcode_11.java
@@ -2022,7 +2022,20 @@ public class Leetcode_11 {
         return true;
     }
 
+    // 应用折扣后的最低总价
     public double minPrice(int[] prices, int[] discounts) {
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        int j = discounts.length - 1;
+        double total = 0.0D;
+        for (int i = prices.length - 1; i >= 0; --i) {
+            if (j >= 0) {
+                total += prices[i] * (100D - discounts[j--]) / 100.0D;
+            } else {
+                total += prices[i];
+            }
+        }
+        return total;
 
     }
 
