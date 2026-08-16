81be856462a3223eb84566b18991b060378f027d
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-08-16 08:25:08 +0800

    2029. 石子游戏 IX (Stone Game IX)

--- Leetcode_11.java
+++ Leetcode_11.java
@@ -2067,4 +2067,29 @@ public class Leetcode_11 {
         return res;
     }
 
+    // 2029. 石子游戏 IX (Stone Game IX)
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnt = new int[3];
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        int n = stones.length;
+        // 小技巧：交换 cnt[1] 和 cnt[2] 再调用 check，相当于 Alice 第一回合移除了 2
+        return check2029(n, cnt.clone()) || check2029(n, new int[] { cnt[0], cnt[2], cnt[1] });
+    }
+
+    private boolean check2029(int n, int[] cnt) {
+        if (cnt[1] == 0) {
+            return false;
+        }
+        cnt[1]--;
+        // 第一回合 Alice 移除 1，后面两人交替移除 1 和 2，中途可以插入 cnt[0] 个 0
+        int rounds = 1 + Math.min(cnt[1], cnt[2]) * 2 + cnt[0];
+        if (cnt[1] > cnt[2]) { // 可以再移除一个 1
+            rounds++;
+        }
+        return rounds < n && rounds % 2 > 0;
+    }
+
 }
