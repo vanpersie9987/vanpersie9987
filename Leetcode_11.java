c63e739abe7c0eea611134f8ee43e5324ca41b2e
liumingyuan <mingyuan.liu@lingyue-digital.com>
2026-08-16 08:31:43 +0800

    4020. 电梯请求 I (Elevator Requests I)

--- Leetcode_11.java
+++ Leetcode_11.java
@@ -2092,4 +2092,14 @@ public class Leetcode_11 {
         return rounds < n && rounds % 2 > 0;
     }
 
+    // 4020. 电梯请求 I (Elevator Requests I)
+    public int elevatorRequests(int n, int[] requests) {
+        int res = requests[0];
+        for (int i = 1; i < requests.length; ++i) {
+            res += Math.abs(requests[i] - requests[i - 1]);
+        }
+        return res;
+
+    }
+
 }
